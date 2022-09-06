337ca6097d6c85cb78bb77ea1bb1b2087f504362
Le Vivilet <levivlet@pm.me>
2022-09-06 16:31:18 +0200

    add file references error e2e test

--- packages/ts-server-e2e-tests/src/sample.file-references-error-no-project.test.js
+++ packages/ts-server-e2e-tests/src/sample.file-references-error-no-project.test.js
@@ -3,10 +3,10 @@ import { TsServer } from 'ts-server'
 import * as TsServerRequests from 'ts-server-requests'
 import { createChild, getFixture } from './_shared.js'
 
-test('sample.file-references', async () => {
+test('sample.file-references-error-no-project', async () => {
   const child = createChild()
   const server = TsServer.create(child)
-  const fixture = getFixture('sample.file-references')
+  const fixture = getFixture('sample.file-references-error-no-project')
   await TsServerRequests.configure(server, {})
   await TsServerRequests.updateOpen(server, {
     openFiles: [
@@ -15,12 +15,11 @@ test('sample.file-references', async () => {
       },
     ],
   })
-  expect(
-    await TsServerRequests.fileReferences(server, {
-      file: join(fixture, 'src', 'calculate.ts'),
+  await expect(
+    TsServerRequests.fileReferences(server, {
+      file: join(fixture, 'src', 'cat.ts'),
     })
-  ).toEqual({
-    refs: [], // TODO should find reference to index.ts file
-    symbolName: `"${join(fixture, 'src', 'calculate.ts')}"`,
-  })
+  ).rejects.toThrowError(
+    new Error('TsServer.fileReferences failed to execute: No Project.')
+  )
 })
