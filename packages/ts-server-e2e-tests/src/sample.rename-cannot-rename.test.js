5ed2c17df821b1a96b53d4275fcce4608914e99d
Le Vivilet <levivlet@pm.me>
2022-09-07 08:26:51 +0200

    fix rename cannot rename e2e test

--- packages/ts-server-e2e-tests/src/sample.rename-cannot-rename.test.js
+++ packages/ts-server-e2e-tests/src/sample.rename-cannot-rename.test.js
@@ -3,7 +3,7 @@ import { TsServer } from 'ts-server'
 import * as TsServerRequests from 'ts-server-requests'
 import { createChild, getFixture } from './_shared.js'
 
-test.skip('sample.rename-cannot-rename', async () => {
+test('sample.rename-cannot-rename', async () => {
   const child = createChild()
   const server = TsServer.create(child)
   const fixture = getFixture('sample.rename-cannot-rename')
