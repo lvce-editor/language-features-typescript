afd2a8bada5f24bc00fe5e6e207af6b81e79492a
Le Vivilet <levivlet@pm.me>
2022-09-07 18:31:41 +0200

    refactor: use real typescript for e2e tests

--- packages/e2e/src/typescript.completion.js
+++ packages/e2e/src/typescript.completion.js
@@ -1,64 +1,7 @@
-const tsServerContent = `
-const Commands = {
-  configure(){
-    return undefined
-  },
-  updateOpen(){
-    return undefined
-  },
-  completionInfo(){
-    return {
-      flags: 0,
-      isGlobalCompletion: true,
-      isMemberCompletion: false,
-      isNewIdentifierLocation: false,
-      entries: [
-        {
-          name: 'AbortController',
-          kind: 'var',
-          kindModifiers: 'declare',
-          sortText: '15'
-        }
-      ]
-    }
-  }
-}
-
-const handleMessageRequest = (message) => {
-  console.log(message)
-  const fn = Commands[message.command]
-  const result = fn(message.arguments)
-  process.send({
-    type: 'response',
-    body: result,
-    success: true,
-    request_seq: message.seq
-  })
-}
-
-const handleMessage = (message) => {
-  switch(message.type){
-    case 'request':
-      return handleMessageRequest(message)
-    case 'response':
-      return handleMessageResponse(message)
-    default:
-      return
-
-  }
-}
-
-process.on('message', handleMessage)
-`
-
 test('typescript.completion', async () => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
   await Workspace.setPath(tmpDir)
-  const tsServerPath = await FileSystem.createExecutable(tsServerContent)
-  await Settings.update({
-    'typescript.tsserverPath': tsServerPath,
-  })
   await FileSystem.writeFile(`${tmpDir}/test.ts`, 'win')
   await Main.openUri(`${tmpDir}/test.ts`)
   await Editor.setCursor(0, 3)
