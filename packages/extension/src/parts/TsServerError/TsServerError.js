ae2c6c07adbd1e06444919f12b50e2a1f398946c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-03 19:29:02 +0200

    refactor: move TsServerError class into separate file (#33)

---
+++ packages/extension/src/parts/TsServerError/TsServerError.js
@@ -0,0 +1,25 @@
+import VError from 'verror'
+
+// TODO somehow tsserver stack is not printed
+export class TsServerError extends VError {
+  // @ts-ignore
+  constructor(message, command) {
+    // console.log({ message })
+    const actualMessage = message.message
+    const lines = actualMessage.split('\n')
+    if (lines.length === 1) {
+      super(`TsServer.${command} failed to execute: ${actualMessage}`)
+      return
+    }
+    const [shortDescription, actualError, ...stack] = lines
+    let betterActualError = actualError
+    if (betterActualError.startsWith('Error: ')) {
+      betterActualError = betterActualError.slice(7)
+    }
+    let betterShortDescription = `TsServer.${command} failed to execute`
+    const tsError = new Error(betterActualError)
+    tsError.stack = [shortDescription, actualError, ...stack].join('\n')
+    // @ts-ignore
+    super(tsError, betterShortDescription)
+  }
+}
