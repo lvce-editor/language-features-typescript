79e29c0888511a74915d00690637f46d1da72c73
Le Vivilet <levivlet@pm.me>
2023-04-03 19:26:04 +0200

    refactor: move TsServerError class into separate file

--- packages/extension/src/parts/TsServerRequests/TsServerRequests.js
+++ packages/extension/src/parts/TsServerRequests/TsServerRequests.js
@@ -1,38 +1,10 @@
 // for reference, see https://github.com/Microsoft/TypeScript/blob/main/lib/protocol.d.ts#L5
 
-import VError from 'verror'
 import * as Id from '../Id/Id.js'
 import * as TsPrimaryServer from '../TsPrimaryServer/TsPrimaryServer.js'
 import * as TsServerCommandType from '../TsServerCommandType/TsServerCommandType.js'
+import { TsServerError } from '../TsServerError/TsServerError.js'
 import * as TsServerMessageType from '../TsServerMessageType/TsServerMessageType.js'
-
-export const state = {
-  seq: 1,
-}
-
-// TODO somehow tsserver stack is not printed
-class TsServerError extends VError {
-  // @ts-ignore
-  constructor(message, command) {
-    // console.log({ message })
-    const actualMessage = message.message
-    const lines = actualMessage.split('\n')
-    if (lines.length === 1) {
-      super(`TsServer.${command} failed to execute: ${actualMessage}`)
-      return
-    }
-    const [shortDescription, actualError, ...stack] = lines
-    let betterActualError = actualError
-    if (betterActualError.startsWith('Error: ')) {
-      betterActualError = betterActualError.slice(7)
-    }
-    let betterShortDescription = `TsServer.${command} failed to execute`
-    const tsError = new Error(betterActualError)
-    tsError.stack = [shortDescription, actualError, ...stack].join('\n')
-    // @ts-ignore
-    super(tsError, betterShortDescription)
-  }
-}
 
 /**
  * @param {import('typescript/lib/protocol').BraceCompletionRequest['arguments']} params
