07263b51fbdffabaf9409dad7ac47ea7d8c47ccc
Le Vivilet <levivlet@pm.me>
2023-04-10 17:55:12 +0200

    fix: references

--- packages/node/src/parts/Refererences/References.js
+++ packages/node/src/parts/Refererences/References.js
@@ -1,6 +1,6 @@
 import * as TsServerRequests from '../TsServerRequests/TsServerRequests.js'
 
 export const getReferences = async (params) => {
-  const tsResult = await TsServerRequests.definition(params)
+  const tsResult = await TsServerRequests.references(params)
   return tsResult
 }
