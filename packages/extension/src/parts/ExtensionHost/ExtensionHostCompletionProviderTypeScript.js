76c9d9c70e56c39956f4f263a44d3650c20af1e9
Le Vivilet <levivlet@pm.me>
2023-04-10 19:10:55 +0200

    refactor: move conversion from ts types to extension types to extension

--- packages/extension/src/parts/ExtensionHost/ExtensionHostCompletionProviderTypeScript.js
+++ packages/extension/src/parts/ExtensionHost/ExtensionHostCompletionProviderTypeScript.js
@@ -1,60 +1,11 @@
-import * as NotUsefulEntries from '../NotUsefuleEntries/NotUsefulEntries.js'
-import * as Rpc from '../Rpc/Rpc.js'
-import * as TsCompletionItemKind from '../TsCompletionItemKind/TsCompletionItemKind.js'
 import * as Assert from '../Assert/Assert.js'
+import * as GetCompletionFromTsResult from '../GetCompletionFromTsResult/GetCompletionFromTsResult.js'
+import * as Rpc from '../Rpc/Rpc.js'
 import * as TextDocumentSync from '../TextDocumentSync/TextDocumentSync.js'
+import * as Position from '../Position/Position.js'
 
 export const languageId = 'typescript'
 
-/**
- *
- * @param {import('typescript/lib/protocol').ScriptElementKind} tsKind
- * @returns
- */
-const convertCompletionItemKind = (tsKind) => {
-  switch (tsKind) {
-    case TsCompletionItemKind.Function:
-      return /* Function */ 3
-    case TsCompletionItemKind.Var:
-      return /* Variable */ 4
-    case TsCompletionItemKind.Keyword:
-      return /* Keyword */ 5
-    case TsCompletionItemKind.Directory:
-      return /* Folder */ 6
-    case TsCompletionItemKind.Script:
-      return /* File */ 7
-    default:
-      return /* Unknown */ 0
-  }
-}
-
-/**
- *
- * @param {import('typescript/lib/protocol').CompletionEntry} tsEntry
- */
-const convertTsCompletionEntry = (tsEntry) => {
-  return {
-    label: tsEntry.name,
-    snippet: tsEntry.name,
-    kind: convertCompletionItemKind(tsEntry.kind),
-  }
-}
-
-/**
- *
- * @param {import('typescript/lib/protocol').CompletionEntry} tsEntry
- */
-const isUsefulEntry = (tsEntry) => {
-  return !NotUsefulEntries.notUsefulEntries.has(tsEntry.name)
-}
-
-/**
- * @param {import('typescript/lib/protocol').CompletionInfo} tsResult
- * @returns {vscode.Completion[]}
- */
-const getCompletionFromTsResult = (tsResult) => {
-  return tsResult.entries.filter(isUsefulEntry).map(convertTsCompletionEntry)
-}
 /**
  * @type {vscode.CompletionProvider['provideCompletions']}
  */
@@ -62,8 +13,13 @@ export const provideCompletions = async (textDocument, offset) => {
   const uri = textDocument.uri
   Assert.string(uri)
   await TextDocumentSync.openTextDocuments([textDocument])
-  const items = await Rpc.invoke('Completion.getCompletion', uri, offset)
-  console.log({ items })
+  const tsPosition = Position.getTsPosition(textDocument, offset)
+  const tsResult = await Rpc.invoke('Completion.getCompletion', {
+    file: textDocument.uri,
+    line: tsPosition.line,
+    offset: tsPosition.offset,
+  })
+  const items = GetCompletionFromTsResult.getCompletionFromTsResult(tsResult)
   return items
 }
 
--- packages/node/src/parts/Completion/Completion.js
+++ packages/node/src/parts/Completion/Completion.js
@@ -1,32 +1,11 @@
-import * as ConvertCompletionEntry from '../ConvertTsCompletionEntry/ConvertTsCompletionEntry.js'
-import * as IsUsefulEntry from '../IsUsefulEntry/IsUsefulEntry.js'
-import * as Position from '../Position/Position.js'
-import * as TextDocuments from '../TextDocuments/TextDocuments.js'
 import * as TsServerRequests from '../TsServerRequests/TsServerRequests.js'
 
 export const languageId = 'typescript'
 
 /**
- * @param {import('typescript/lib/protocol').CompletionInfo} tsResult
- * @returns {vscode.Completion[]}
- */
-const getCompletionFromTsResult = (tsResult) => {
-  return tsResult.entries
-    .filter(IsUsefulEntry.isUsefulEntry)
-    .map(ConvertCompletionEntry.convertTsCompletionEntry)
-}
-/**
  * @type {vscode.CompletionProvider['provideCompletions']}
  */
-export const getCompletion = async (uri, offset) => {
-  if (!TextDocuments.hasUri(uri)) {
-    throw new Error(`Text document must be opened before requesting completion`)
-  }
-  const tsPosition = Position.getTsPosition(uri, offset)
-  const tsResult = await TsServerRequests.completionInfo({
-    file: uri,
-    line: tsPosition.line,
-    offset: tsPosition.offset,
-  })
-  return getCompletionFromTsResult(tsResult)
+export const getCompletion = async (params) => {
+  const tsResult = await TsServerRequests.completionInfo(params)
+  return tsResult
 }
