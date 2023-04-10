27a03f1a365f1f2ce7acaa58bc28688b85ceae8b
Le Vivilet <levivlet@pm.me>
2023-04-10 15:48:51 +0200

    refactor: move getTsserverPath into separate function

--- packages/extension/src/languageFeaturesTypeScriptMain.js
+++ packages/extension/src/languageFeaturesTypeScriptMain.js
@@ -1,9 +1,10 @@
 import * as CompletionProvider from './parts/ExtensionHost/ExtensionHostCompletionProviderTypeScript.js'
+import * as GetTsServerPath from './parts/GetTsServerPath/GetTsServerPath.js'
 import * as Rpc from './parts/Rpc/Rpc.js'
 
 export const activate = async ({ path }) => {
   await Rpc.listen({ path })
-  const tsServerPath = await Rpc.invoke('GetTsServerPath.getTsServerPath')
+  const tsServerPath = await GetTsServerPath.getTsServerPath()
   await Rpc.invoke('Initialize.initialize', {
     tsServerPath,
   })
