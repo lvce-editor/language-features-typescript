29e69890e3ed94a2c9ed04b1d0a9180a26092055
Le Vivilet <levivlet@pm.me>
2023-06-04 19:15:34 +0200

    refactor: update completion kind enum

--- packages/extension/src/parts/CompletionKind/CompletionKind.js
+++ packages/extension/src/parts/CompletionKind/CompletionKind.js
@@ -1,9 +1,9 @@
+export const Field = vscode.EditorCompletionType.Field
+export const File = vscode.EditorCompletionType.File
+export const Folder = vscode.EditorCompletionType.Folder
+export const Function = vscode.EditorCompletionType.Function
+export const Keyword = vscode.EditorCompletionType.Keyword
 export const Property = vscode.EditorCompletionType.Property
 export const Value = vscode.EditorCompletionType.Value
-export const Function = vscode.EditorCompletionType.Function
 export const Variable = vscode.EditorCompletionType.Variable
-export const Keyword = vscode.EditorCompletionType.Keyword
-export const Folder = vscode.EditorCompletionType.Folder
-export const File = vscode.EditorCompletionType.File
 export const Unknown = 0
-export const Field = 8
