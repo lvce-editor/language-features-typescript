c7ac1cec696b31ef1166b9aedc518c6d6ced06e7
Le Vivilet <levivlet@pm.me>
2023-06-06 00:45:22 +0200

    refactor: move getTsServerArgv function into separate file

--- packages/extension/src/languageFeaturesTypeScriptMain.js
+++ packages/extension/src/languageFeaturesTypeScriptMain.js
@@ -2,6 +2,7 @@ import * as GetTsServerPath from './parts/GetTsServerPath/GetTsServerPath.js'
 import * as Providers from './parts/Providers/Providers.js'
 import * as RegisterProviders from './parts/RegisterProviders/RegisterProviders.js'
 import * as Rpc from './parts/Rpc/Rpc.js'
+import * as GetTsServerArgv from './parts/GetTsServerArgv/GetTsServerArgv.js'
 
 const toJsProvider = (provider) => {
   return {
@@ -10,32 +11,11 @@ const toJsProvider = (provider) => {
   }
 }
 
-const getArgv = ({ disableAutomaticTypingAcquisition = true, npmLocation = '' }) => {
-  const argv = []
-  argv.push('--useInferredProjectPerProjectRoot')
-  if (disableAutomaticTypingAcquisition) {
-    argv.push('--disableAutomaticTypingAcquisition')
-  }
-  if (npmLocation) {
-    argv.push('--npmLocation', `${npmLocation}`)
-  }
-  argv.push('--locale', 'en')
-  argv.push('--noGetErrOnBackgroundUpdate')
-  argv.push('--validateDefaultNpmLocation')
-  argv.push('--suppressDiagnosticEvents')
-
-  const execArgv = ['--max-old-space-size=200']
-  return {
-    argv,
-    execArgv,
-  }
-}
-
 export const activate = async ({ path }) => {
   await Rpc.listen({ path })
   const tsServerPath = await GetTsServerPath.getTsServerPath()
   const options = {}
-  const { argv, execArgv } = getArgv(options)
+  const { argv, execArgv } = GetTsServerArgv.getTsServerArgv(options)
   await Rpc.invoke('Initialize.initialize', {
     tsServerPath,
     argv,
