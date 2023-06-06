8a9137d476d849e9651fdf7e1f7fa908ed3847b9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-06-06 15:38:26 +0200

    feature: add support for more ts completion item kinds (#57)

--- packages/extension/src/parts/ConvertCompletionItemKind/ConvertCompletionItemKind.js
+++ packages/extension/src/parts/ConvertCompletionItemKind/ConvertCompletionItemKind.js
@@ -10,11 +10,16 @@ export const convertCompletionItemKind = (tsKind) => {
   switch (tsKind) {
     case TsCompletionItemKind.Function:
     case TsCompletionItemKind.Method:
+    case TsCompletionItemKind.LocalFunction:
       return CompletionItemKind.Function
     case TsCompletionItemKind.Var:
     case TsCompletionItemKind.Const:
     case TsCompletionItemKind.Let:
+    case TsCompletionItemKind.LocalVariable:
+    case TsCompletionItemKind.Alias:
+    case TsCompletionItemKind.Parameter:
       return CompletionItemKind.Variable
+    case TsCompletionItemKind.PrimitiveType:
     case TsCompletionItemKind.Keyword:
       return CompletionItemKind.Keyword
     case TsCompletionItemKind.Directory:
@@ -22,6 +27,9 @@ export const convertCompletionItemKind = (tsKind) => {
     case TsCompletionItemKind.Script:
       return CompletionItemKind.File
     case TsCompletionItemKind.Property:
+    case TsCompletionItemKind.MemberVariable:
+    case TsCompletionItemKind.MemberGetAccessor:
+    case TsCompletionItemKind.MemberSetAccessor:
       return CompletionItemKind.Field
     default:
       return CompletionItemKind.Unknown
