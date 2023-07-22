42be29b35ea1d4af2d4726f53d413845e2400e29
Le Vivilet <levivlet@pm.me>
2023-07-22 12:16:07 +0200

    feature: show deprecated completion items as deprecated

--- packages/extension/src/parts/ConvertTsCompletionEntry/ConvertTsCompletionEntry.js
+++ packages/extension/src/parts/ConvertTsCompletionEntry/ConvertTsCompletionEntry.js
@@ -1,4 +1,5 @@
 import * as ConvertCompletionItemKind from '../ConvertCompletionItemKind/ConvertCompletionItemKind.js'
+import * as ConvertTsCompletionKindModifiers from '../ConvertTsCompletionKindModifiers/ConvertTsCompletionKindModifiers.js'
 
 // TODO don't pass lots of data to renderer worker
 // source and data properties are only necessary for resolveCompletionItem
@@ -8,12 +9,13 @@ import * as ConvertCompletionItemKind from '../ConvertCompletionItemKind/Convert
  * @param {import('../TsServerProtocol/TsServerProtocol.js').CompletionEntry} tsEntry
  */
 export const convertTsCompletionEntry = (tsEntry) => {
-  const { name, kind, source, data } = tsEntry
+  const { name, kind, source, data, kindModifiers } = tsEntry
   return {
     label: name,
     snippet: name,
     kind: ConvertCompletionItemKind.convertCompletionItemKind(kind),
     source,
     data,
+    flags: ConvertTsCompletionKindModifiers.convertTsCompletionKindModifiers(kindModifiers),
   }
 }
---
+++ packages/extension/src/parts/ConvertTsCompletionKindModifiers/ConvertTsCompletionKindModifiers.js
@@ -0,0 +1,19 @@
+import * as CompletionItemFlags from '../CompletionItemFlags/CompletionItemFlags.js'
+
+/**
+ * @param {string} modifier
+ */
+export const convertTsCompletionKindModifiers = (modifier) => {
+  const parts = modifier.split(',')
+  let flag = CompletionItemFlags.None
+  for (const part of parts) {
+    switch (part) {
+      case 'deprecated':
+        flag |= CompletionItemFlags.Deprecated
+        break
+      default:
+        break
+    }
+  }
+  return flag
+}
