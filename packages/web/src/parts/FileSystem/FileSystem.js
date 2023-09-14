2214863a3fd3bfa37b8a85e28b2a43eb01917b53
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-09-14 21:26:25 +0200

    feature: add function to create typescript language service host (#85)

---
+++ packages/web/src/parts/FileSystem/FileSystem.js
@@ -0,0 +1,14 @@
+export const readFile = (fileName) => {
+  console.log({ fileName })
+  return ''
+}
+
+export const version = 1
+
+export const getAllFiles = () => {
+  return []
+}
+
+export const readVersion = (fileName) => {
+  return '1'
+}
--- packages/web/src/parts/Initialize/Initialize.js
+++ packages/web/src/parts/Initialize/Initialize.js
@@ -1,10 +1,14 @@
 import * as GetTypeScriptPath from '../GetTypeScriptPath/GetTypeScriptPath.js'
 import * as LoadTypeScript from '../LoadTypeScript/LoadTypeScript.js'
+import * as TypeScriptLanguageHost from '../TypeScriptLanguageHost/TypeScriptLanguageHost.js'
 
 export const initialize = async () => {
   const typescriptPath = GetTypeScriptPath.getTypeScriptPath()
   const typescript = await LoadTypeScript.loadTypeScript(typescriptPath)
   console.log({ typescript })
+  const languageServiceHost = TypeScriptLanguageHost.create(typescript)
+  console.log({ typescript })
+  // const languageService = typescript.createLanguageService(languageServiceHost, undefined, false)
   // TODO
   console.log('initialize')
 }
