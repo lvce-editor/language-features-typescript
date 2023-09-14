f7552b6331095e139e0d43c61ce78debe5962c04
Le Vivilet <levivlet@pm.me>
2023-09-14 21:11:07 +0200

    feature: load typescript and add completion command

--- packages/web/src/parts/CommandMap/CommandMap.js
+++ packages/web/src/parts/CommandMap/CommandMap.js
@@ -1,9 +1,13 @@
+import * as Completion from '../Completion/Completion.js'
+import * as Configure from '../Configure/Configure.js'
 import * as GetTsServerPath from '../GetTsServerPath/GetTsServerPath.js'
 import * as Initialize from '../Initialize/Initialize.js'
-import * as Configure from '../Configure/Configure.js'
+import * as UpdateOpen from '../UpdateOpen/UpdateOpen.js'
 
 export const commandMap = {
   'GetTsServerPath.getTsServerPath': GetTsServerPath.getTsServerPath,
   'Initialize.initialize': Initialize.initialize,
   'Configure.configure': Configure.configure,
+  'UpdateOpen.updateOpen': UpdateOpen.updateOpen,
+  'Completion.getCompletion': Completion.getCompletion,
 }
--- packages/web/src/parts/Initialize/Initialize.js
+++ packages/web/src/parts/Initialize/Initialize.js
@@ -1,4 +1,10 @@
-export const initialize = () => {
+import * as GetTypeScriptPath from '../GetTypeScriptPath/GetTypeScriptPath.js'
+import * as LoadTypeScript from '../LoadTypeScript/LoadTypeScript.js'
+
+export const initialize = async () => {
+  const typescriptPath = GetTypeScriptPath.getTypeScriptPath()
+  const typescript = await LoadTypeScript.loadTypeScript(typescriptPath)
+  console.log({ typescript })
   // TODO
   console.log('initialize')
 }
