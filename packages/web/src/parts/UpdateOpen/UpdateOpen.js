4e2f7f199cfea3fe1d9a55f69a229a6ddbb3bf99
Le Vivilet <levivlet@pm.me>
2023-09-14 21:53:47 +0200

    feature: improve language host

--- packages/web/src/parts/UpdateOpen/UpdateOpen.js
+++ packages/web/src/parts/UpdateOpen/UpdateOpen.js
@@ -1,5 +1,7 @@
 import * as FileSystem from '../FileSystem/FileSystem.js'
 
-export const updateOpen = (options) => {
-  FileSystem.writeFile(options.uri, options.text)
+export const updateOpen = (files) => {
+  for (const file of files) {
+    FileSystem.writeFile(file.uri, file.text)
+  }
 }
