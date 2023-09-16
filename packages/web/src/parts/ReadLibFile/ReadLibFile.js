bb6f6d5067bd07120733ab8bb979155381be0e49
Le Vivilet <levivlet@pm.me>
2023-09-16 12:40:16 +0200

    refactor: move reading libFile into separate function

---
+++ packages/web/src/parts/ReadLibFile/ReadLibFile.js
@@ -0,0 +1,15 @@
+import * as GetTextSync from '../GetTextSync/GetTextSync.js'
+
+export const readLibFile = (uri) => {
+  if (uri === '/lib.d.ts') {
+    const libUrl = new URL('../../../../extension/node_modules/typescript/lib/lib.d.ts', import.meta.url).toString()
+    const text = GetTextSync.getTextSync(libUrl)
+    return text
+  }
+  if (uri.startsWith('/node_modules/@typescript')) {
+    const relativePath = uri.replace('-', '.').slice('/node_modules/@typescript'.length, -3) + '.d.ts'
+    const libUrl = new URL(`../../../../extension/node_modules/typescript/lib${relativePath}`, import.meta.url).toString()
+    const text = GetTextSync.getTextSync(libUrl)
+    return text
+  }
+}
