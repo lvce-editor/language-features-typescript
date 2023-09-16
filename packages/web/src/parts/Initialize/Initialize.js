a527b1317caa8abd426ef2aee1d6a4775d9ed8fc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-09-16 13:03:56 +0200

    feature: add separate loadTsServer function (#103)

--- packages/web/src/parts/Initialize/Initialize.js
+++ packages/web/src/parts/Initialize/Initialize.js
@@ -1,7 +1,7 @@
 import * as GetTsServerLibraryPath from '../GetTsServerLibraryPath/GetTsServerLibraryPath.js'
 import * as GetTypeScriptPath from '../GetTypeScriptPath/GetTypeScriptPath.js'
-import * as ImportScript from '../ImportScript/ImportScript.js'
 import * as LanguageServiceState from '../LanguageServiceState/LanguageServiceState.js'
+import * as LoadTsServerLibrary from '../LoadTsServerLibrary/LoadTsServerLibrary.js'
 import * as LoadTypeScript from '../LoadTypeScript/LoadTypeScript.js'
 import * as TypeScriptLanguageHost from '../TypeScriptLanguageHost/TypeScriptLanguageHost.js'
 
@@ -12,5 +12,6 @@ export const initialize = async () => {
   const languageService = typescript.createLanguageService(languageServiceHost, undefined, false)
   LanguageServiceState.set(languageService)
   const tsServerLibraryPath = GetTsServerLibraryPath.getTsServerLibraryPath()
-  const tsServerLibrary = await ImportScript.importScript(tsServerLibraryPath)
+  const tsServerLibrary = await LoadTsServerLibrary.loadTsServerLibrary(tsServerLibraryPath)
+  console.log({ tsServerLibrary })
 }
---
+++ packages/web/src/parts/LoadTsServerLibrary/LoadTsServerLibrary.js
@@ -0,0 +1,18 @@
+import * as ImportScript from '../ImportScript/ImportScript.js'
+
+/**
+ *
+ * @param {string} path
+ * @returns
+ */
+export const loadTsServerLibrary = async (path) => {
+  try {
+    const module = await ImportScript.importScript(path)
+    if (!module || !module.ts) {
+      throw new Error(`no ts export found`)
+    }
+    return module.ts
+  } catch (error) {
+    throw new Error(`Failed to load tsserver library: ${error}`)
+  }
+}
