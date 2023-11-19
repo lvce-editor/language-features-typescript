83976da13489eadbb78d54a1aa584a635cf1ae10
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-19 16:51:53 +0100

    fix: getLibFileUrl in web (#137)

---
+++ packages/web/src/parts/GetLibFileUrl/GetLibFileUrl.js
@@ -0,0 +1,10 @@
+export const getLibFileUrl = (uri) => {
+  if (uri === '/lib.d.ts') {
+    return new URL('../../../../extension/node_modules/typescript/lib/lib.d.ts', import.meta.url).toString()
+  }
+  if (uri.startsWith('/node_modules/@typescript/lib')) {
+    const relativePath = uri.slice('/node_modules/@typescript/lib'.length, -3).replaceAll('-', '.').replaceAll('/', '.') + '.d.ts'
+    return new URL(`../../../../extension/node_modules/typescript/lib/lib${relativePath}`, import.meta.url).toString()
+  }
+  return ''
+}
