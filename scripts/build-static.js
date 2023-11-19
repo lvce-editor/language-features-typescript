4d029124c0b7f501878fa974aa11f92cb0c1fc17
Le Vivilet <levivlet@pm.me>
2023-11-19 17:00:56 +0100

    feature: add file with error for testing web

--- scripts/build-static.js
+++ scripts/build-static.js
@@ -125,10 +125,33 @@ for (const dirent of ['README.md', 'LICENSE.txt', 'package.json']) {
   await cp(join(typeScriptPath, dirent), join(root, 'dist', commitHash, 'extensions', 'builtin.language-features-typescript', 'typescript', dirent))
 }
 
-const defaultSettingsPath = join(root, 'dist', commitHash, 'config', 'defaultSettings.json')
-const oldSettings = JSON.parse(readFileSync(defaultSettingsPath, 'utf8'))
-const newSettings = {
-  ...oldSettings,
-  'editor.diagnostics': true,
+const updateJson = (path, update) => {
+  const oldJson = JSON.parse(readFileSync(path, 'utf8'))
+  const newJson = update(oldJson)
+  writeFileSync(path, JSON.stringify(newJson, null, 2) + '\n')
 }
-writeFileSync(defaultSettingsPath, JSON.stringify(newSettings, null, 2) + '\n')
+
+const updateSettings = (oldSettings) => {
+  return {
+    ...oldSettings,
+    'editor.diagnostics': true,
+  }
+}
+const defaultSettingsPath = join(root, 'dist', commitHash, 'config', 'defaultSettings.json')
+updateJson(defaultSettingsPath, updateSettings)
+
+const fileMapPath = join(root, 'dist', commitHash, 'config', 'fileMap.json')
+const updateFileMap = (oldFileMap) => {
+  return [...oldFileMap, '/playground/languages/error.ts']
+}
+updateJson(fileMapPath, updateFileMap)
+
+const errorPath = join(root, 'dist', commitHash, 'playground', 'languages', 'error.ts')
+writeFileSync(
+  errorPath,
+  `const add = (a,b) => {
+  return a + b
+}
+
+add(1,2,3,4)`
+)
