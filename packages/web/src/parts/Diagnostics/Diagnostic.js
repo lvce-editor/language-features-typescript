c2538c5c4ac6b3bc9105a719140f19dc018c5d79
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-19 11:00:55 +0100

    feature: display web diagnostics (#131)

--- packages/web/src/parts/Diagnostics/Diagnostic.js
+++ packages/web/src/parts/Diagnostics/Diagnostic.js
@@ -1,9 +1,28 @@
 import * as LanguageServiceState from '../LanguageServiceState/LanguageServiceState.js'
 
+const getDiagnosticFromTsResult = (diagnostic) => {
+  return {
+    start: {
+      line: 1,
+      offset: 1,
+    },
+    end: {
+      line: 1,
+      offset: 5,
+    },
+    text: diagnostic.messageText,
+  }
+}
+
+const getDiagnosticsFromTsResult = (tsResult) => {
+  return tsResult.map(getDiagnosticFromTsResult)
+}
+
 export const getDiagnostics = (params) => {
   console.log({ params })
   const { file } = params
   const languageService = LanguageServiceState.get()
   const tsResult = languageService.getSemanticDiagnostics(file)
-  return tsResult
+  const diagnostics = getDiagnosticsFromTsResult(tsResult)
+  return diagnostics
 }
