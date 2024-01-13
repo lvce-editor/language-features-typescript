fd994e49afb83f8efbf49f2853e9622a6d0ff599
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-13 23:30:51 +0100

    feature: return organizeImport edits (#164)

--- packages/extension/src/parts/ExtensionHost/ExtensionHostCodeActionsProviderTypeScript.js
+++ packages/extension/src/parts/ExtensionHost/ExtensionHostCodeActionsProviderTypeScript.js
@@ -2,18 +2,33 @@ import * as OrganizeImports from '../OrganizeImports/OrganizeImports.js'
 
 export const languageId = 'typescript'
 
+const getEditsFromTsResult = (textDocument, tsResult) => {
+  // TODO handle case when edits are for a different file
+  const edits = []
+  for (const item of tsResult) {
+    for (const textChange of item.textChanges) {
+      const { start, end, newText } = textChange
+      const startOffset = vscode.getOffset(textDocument, { rowIndex: start.line - 1, columnIndex: start.offset - 1 })
+      const endOffset = vscode.getOffset(textDocument, {
+        rowIndex: end.line - 1,
+        columnIndex: end.offset - 1,
+      })
+      edits.push({
+        startOffset,
+        endOffset,
+        inserted: newText,
+      })
+    }
+  }
+  return edits
+}
+
 const organizeImports = {
   kind: 'source.organizeImports', // TODO use numeric code action type
   name: 'Organize Imports',
   async execute(textDocument) {
-    console.log({ textDocument })
     const tsResult = await OrganizeImports.organizeImports(textDocument)
-    console.log({ tsResult })
-    /**
-     * @type {any[]}
-     */
-    const edits = []
-    // TODO ask tsserver for organize imports edits
+    const edits = getEditsFromTsResult(textDocument, tsResult)
     return edits
   },
 }
