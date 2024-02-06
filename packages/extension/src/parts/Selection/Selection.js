359d81026c76dd435d0c6d8de15320f59be4ee22
Le Vivilet <levivlet@pm.me>
2024-02-06 22:25:48 +0100

    fix: grow selection from array

--- packages/extension/src/parts/Selection/Selection.js
+++ packages/extension/src/parts/Selection/Selection.js
@@ -5,10 +5,23 @@ const getPositionsFromTsResult = (tsResult) => {
   if (!tsResult || tsResult.length === 0) {
     return []
   }
-  const [first] = tsResult
-  const { textSpan } = first
-  const { start, end } = textSpan
-  return [start.line - 1, start.offset - 1, end.line - 1, end.offset - 1]
+  let minLine = Infinity
+  let minLineColumn = Infinity
+  let maxLine = 0
+  let maxLineColumn = 0
+  for (const element of tsResult) {
+    const { textSpan } = element
+    const { start, end } = textSpan
+    if (start.line < minLine) {
+      minLine = start.line
+      minLineColumn = start.offset
+    }
+    if (end.line > maxLine) {
+      maxLine = end.line
+      maxLineColumn = end.offset
+    }
+  }
+  return [minLine - 1, minLineColumn - 1, maxLine - 1, maxLineColumn - 1]
 }
 
 const getLocations = (positions) => {
@@ -22,7 +35,7 @@ const getLocations = (positions) => {
       line: positions[i] + 1,
       offset: positions[i + 1] + 1,
     }
-    if (next.line === last.line && next.column === last.column) {
+    if (next.line === last.line && next.offset === last.offset) {
       continue
     }
     last = next
@@ -39,5 +52,6 @@ export const expandSelection = async (textDocument, positions) => {
     locations,
   })
   const newPositions = getPositionsFromTsResult(tsResult)
+  console.log({ positions, locations, tsResult })
   return newPositions
 }
