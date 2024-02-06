65787e0d719195cc67d797e7ef3d82ba5533d036
Le Vivilet <levivlet@pm.me>
2024-02-06 22:04:38 +0100

    remove log

--- packages/node/src/parts/Selection/Selection.js
+++ packages/node/src/parts/Selection/Selection.js
@@ -2,6 +2,5 @@ import * as TsServerRequests from '../TsServerRequests/TsServerRequests.js'
 
 export const expandSelection = async (params) => {
   const tsResult = await TsServerRequests.selectionRange(params)
-  console.log({ params, tsResult })
   return tsResult
 }
