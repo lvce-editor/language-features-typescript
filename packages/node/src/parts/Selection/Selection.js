149096572791e48e7c7c327e8e86fcf69b3546a6
Le Vivilet <levivlet@pm.me>
2024-02-06 23:35:27 +0100

    refactor: remove console log

--- packages/node/src/parts/Selection/Selection.js
+++ packages/node/src/parts/Selection/Selection.js
@@ -1,7 +1,6 @@
 import * as TsServerRequests from '../TsServerRequests/TsServerRequests.js'
 
 export const expandSelection = async (params) => {
-  console.log({ params: params.locations })
   const tsResult = await TsServerRequests.selectionRange(params)
   return tsResult
 }
