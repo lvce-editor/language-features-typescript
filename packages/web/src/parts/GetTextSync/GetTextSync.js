c962798e83f0fe90e8ed31f817225dfc37881cf9
Le Vivilet <levivlet@pm.me>
2024-02-22 20:57:58 +0100

    feature: set accept text/plain header for text requests

--- packages/web/src/parts/GetTextSync/GetTextSync.js
+++ packages/web/src/parts/GetTextSync/GetTextSync.js
@@ -1,5 +1,6 @@
 export const getTextSync = (url) => {
   const xhr = new XMLHttpRequest()
+  xhr.setRequestHeader('Accept', 'text/plain')
   xhr.open('GET', /* url */ url, /* async */ false)
   xhr.send(null)
   return xhr.responseText
