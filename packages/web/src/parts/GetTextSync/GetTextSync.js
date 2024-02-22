c59f7520dbdec5c94e23127019b5521b3213da0a
Le Vivilet <levivlet@pm.me>
2024-02-22 21:04:47 +0100

    fix: order for xml http request

--- packages/web/src/parts/GetTextSync/GetTextSync.js
+++ packages/web/src/parts/GetTextSync/GetTextSync.js
@@ -1,7 +1,7 @@
 export const getTextSync = (url) => {
   const xhr = new XMLHttpRequest()
-  xhr.setRequestHeader('Accept', 'text/plain')
   xhr.open('GET', /* url */ url, /* async */ false)
+  xhr.setRequestHeader('Accept', 'text/plain')
   xhr.send(null)
   return xhr.responseText
 }
