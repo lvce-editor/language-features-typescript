2acceebadc1bb82170474e16b63d835d2a7bd4b7
Le Vivilet <levivlet@pm.me>
2024-04-06 18:26:03 +0200

    feature: use async functions for build

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -1,62 +1,27 @@
-import { packageExtension, bundleJs, replace } from '@lvce-editor/package-extension'
+import { bundleJs, packageExtension, replace } from '@lvce-editor/package-extension'
 import { execSync } from 'child_process'
-import fs, { cpSync, readFileSync, writeFileSync } from 'fs'
+import { copyFile, cp, mkdir, rm, writeFile, readFile } from 'node:fs/promises'
 import path, { join } from 'path'
 import { root } from './root.js'
-
-const NOT_NEEDED = [
-  'node_modules/minimist/.github',
-  'node_modules/minimist/example',
-  'node_modules/minimist/test',
-  'node_modules/minimist/.eslintrc',
-  'node_modules/minimist/.nycrc',
-  'node_modules/minimist/CHANGELOG.md',
-  'node_modules/typescript/bin',
-  'node_modules/typescript/loc',
-  'node_modules/typescript/AUTHORS.md',
-  'node_modules/typescript/CODE_OF_CONDUCT.md',
-  'node_modules/typescript/LICENSE.txt',
-  'node_modules/typescript/SECURITY.md',
-  'node_modules/typescript/lib/cs',
-  'node_modules/typescript/lib/de',
-  'node_modules/typescript/lib/es',
-  'node_modules/typescript/lib/fr',
-  'node_modules/typescript/lib/it',
-  'node_modules/typescript/lib/ja',
-  'node_modules/typescript/lib/ko',
-  'node_modules/typescript/lib/pl',
-  'node_modules/typescript/lib/pt-br',
-  'node_modules/typescript/lib/ru',
-  'node_modules/typescript/lib/tr',
-  'node_modules/typescript/lib/zh-cn',
-  'node_modules/typescript/lib/zh-tw',
-  'node_modules/typescript/lib/README.md',
-  'node_modules/typescript/lib/tsc.js',
-  'node_modules/typescript/lib/tsserverlibrary.d.ts',
-  'node_modules/typescript/lib/typescript.d.ts',
-  'node_modules/typescript/lib/typescriptServices.js',
-  'node_modules/typescript/lib/typescriptServices.d.ts',
-  'node_modules/typescript/lib/protocol.d.ts',
-  'node_modules/typescript/lib/typingsInstaller.js',
-]
+import { removeUnusedTypeScriptFiles } from './removeUnusedTypeScriptFIles.js'
 
 const extension = path.join(root, 'packages', 'extension')
 const node = path.join(root, 'packages', 'node')
 const dist = join(root, 'dist')
 
-fs.rmSync(dist, { recursive: true, force: true })
+await rm(dist, { recursive: true, force: true })
 
-fs.mkdirSync(dist)
+await mkdir(dist)
 
-const packageJson = JSON.parse(readFileSync(join(extension, 'package.json')).toString())
+const packageJson = JSON.parse(await readFile(join(extension, 'package.json'), 'utf8'))
 delete packageJson.jest
 delete packageJson.devDependencies
 
-fs.writeFileSync(join(dist, 'package.json'), JSON.stringify(packageJson, null, 2) + '\n')
-fs.copyFileSync(join(root, 'README.md'), join(dist, 'README.md'))
-fs.copyFileSync(join(extension, 'icon.png'), join(dist, 'icon.png'))
-fs.copyFileSync(join(extension, 'extension.json'), join(dist, 'extension.json'))
-fs.cpSync(join(extension, 'src'), join(dist, 'src'), {
+await writeFile(join(dist, 'package.json'), JSON.stringify(packageJson, null, 2) + '\n')
+await copyFile(join(root, 'README.md'), join(dist, 'README.md'))
+await copyFile(join(extension, 'icon.png'), join(dist, 'icon.png'))
+await copyFile(join(extension, 'extension.json'), join(dist, 'extension.json'))
+await cp(join(extension, 'src'), join(dist, 'src'), {
   recursive: true,
 })
 
@@ -75,29 +40,27 @@ const getDependencies = (cwd) => {
   return lines.slice(1, -1)
 }
 
-const copyDependencies = (from, to) => {
+const copyDependencies = async (from, to) => {
   const dependencies = getDependencies(from)
   for (const dependency of dependencies) {
-    fs.cpSync(dependency, join(dist, to, dependency.slice(from.length)), {
+    await cp(dependency, join(dist, to, dependency.slice(from.length)), {
       recursive: true,
     })
   }
 }
 
-copyDependencies(extension, '')
+await copyDependencies(extension, '')
 
-copyDependencies(node, 'node')
+await copyDependencies(node, 'node')
 
-for (const notNeeded of NOT_NEEDED) {
-  fs.rmSync(join(dist, 'node', notNeeded), { force: true, recursive: true })
-}
+await removeUnusedTypeScriptFiles(join(dist, 'node'))
 
-cpSync(join(root, 'packages', 'node', 'src'), join(dist, 'node', 'src'), {
+await cp(join(root, 'packages', 'node', 'src'), join(dist, 'node', 'src'), {
   recursive: true,
 })
-cpSync(join(root, 'packages', 'node', 'package.json'), join(dist, 'node', 'package.json'))
+await cp(join(root, 'packages', 'node', 'package.json'), join(dist, 'node', 'package.json'))
 
-cpSync(join(root, 'packages', 'web', 'src'), join(dist, 'web', 'src'), {
+await cp(join(root, 'packages', 'web', 'src'), join(dist, 'web', 'src'), {
   recursive: true,
 })
 
