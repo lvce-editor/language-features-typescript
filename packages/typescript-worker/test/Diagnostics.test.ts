3b9c89cd6c4a894905e7662829f387842c14fcbe
Le Vivilet <levivlet@pm.me>
2024-04-06 20:39:13 +0200

    test: add diagnostics test

--- packages/typescript-worker/test/Diagnostics.test.ts
+++ packages/typescript-worker/test/Diagnostics.test.ts
@@ -1,15 +1,52 @@
-import * as Diagnostics from '../src/parts/Diagnostics/Diagnostics.ts'
-import { test, expect } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
 
-test.skip('diagnostics', async () => {
+import { beforeEach } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/TypeScriptRpc/TypeScriptRpc.ts', () => {
+  return {
+    invoke: jest.fn(),
+  }
+})
+
+const Diagnostics = await import('../src/parts/Diagnostics/Diagnostics.ts')
+const TypeScriptRpc = await import('../src/parts/TypeScriptRpc/TypeScriptRpc.ts')
+
+test('diagnostics', async () => {
+  jest.spyOn(TypeScriptRpc, 'invoke').mockImplementation(async (method) => {
+    if (method === 'Diagnostic.getDiagnostics') {
+      return [
+        {
+          category: 'error',
+          end: {
+            line: 1,
+            offset: 1,
+          },
+          start: {
+            line: 1,
+            offset: 1,
+          },
+          text: 'test text',
+        },
+      ]
+    }
+  })
   const textDocument = {
     uri: '',
   }
   expect(await Diagnostics.getDiagnostics(textDocument)).toEqual([
     {
+      code: undefined,
       columnIndex: 0,
-      message: 'test error 1',
+      endColumnIndex: 0,
+      endRowIndex: 0,
+      message: 'test text',
       rowIndex: 0,
+      source: 'ts',
+      type: 'error',
       uri: '',
     },
   ])
