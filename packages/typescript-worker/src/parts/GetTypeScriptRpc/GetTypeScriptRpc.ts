3576725629f380d843b7a1280faaff83bc7a693f
Le Vivilet <levivlet@pm.me>
2024-04-06 16:05:44 +0200

    feature: forward diagnostics requests to typescript

--- packages/typescript-worker/src/parts/GetTypeScriptRpc/GetTypeScriptRpc.ts
+++ packages/typescript-worker/src/parts/GetTypeScriptRpc/GetTypeScriptRpc.ts
@@ -1,3 +1,5 @@
+import * as Rpc from '../Rpc/Rpc.ts'
+
 export const getRpc = (path) => {
   // TODO
   // when running in node create websocket rpc
--- packages/typescript-worker/src/parts/TypeScriptRpc/TypeScriptRpc.ts
+++ packages/typescript-worker/src/parts/TypeScriptRpc/TypeScriptRpc.ts
@@ -1,14 +1,5 @@
-import * as GetTypeScriptRpc from '../GetTypeScriptRpc/GetTypeScriptRpc.ts'
-
-export const state = {
-  rpc: undefined,
-}
-
-export const listen = async ({ path }) => {
-  state.rpc = await GetTypeScriptRpc.getRpc(path)
-}
+import * as Rpc from '../Rpc/Rpc.ts'
 
 export const invoke = async (method, ...params) => {
-  const result = await state.rpc.invoke(method, ...params)
-  return result
+  return Rpc.invoke('TypeScriptRpc.invoke', method, ...params)
 }
