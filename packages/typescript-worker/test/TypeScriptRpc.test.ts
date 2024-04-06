1ea4c08e2c4ad4a76e82d2a8b8a450a4acd3553f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-06 20:49:08 +0200

    feature: add rpc test (#235)

---
+++ packages/typescript-worker/test/TypeScriptRpc.test.ts
@@ -0,0 +1,29 @@
+import { expect, jest, test } from '@jest/globals'
+
+import { beforeEach } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/Rpc/Rpc.ts', () => {
+  return {
+    invoke: jest.fn(),
+  }
+})
+
+const Rpc = await import('../src/parts/Rpc/Rpc.ts')
+const TypeScriptRpc = await import('../src/parts/TypeScriptRpc/TypeScriptRpc.ts')
+
+test('listen', async () => {
+  const path = ''
+  await TypeScriptRpc.listen(path)
+  expect(Rpc.invoke).toHaveBeenCalledTimes(1)
+  expect(Rpc.invoke).toHaveBeenCalledWith('TypeScriptRpc.listen', '')
+})
+
+test('invoke', async () => {
+  await TypeScriptRpc.invoke('Completion.getCompletion')
+  expect(Rpc.invoke).toHaveBeenCalledTimes(1)
+  expect(Rpc.invoke).toHaveBeenCalledWith('TypeScriptRpc.invoke', 'Completion.getCompletion')
+})
