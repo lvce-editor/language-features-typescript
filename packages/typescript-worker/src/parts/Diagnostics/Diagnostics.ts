527bcc073a221e65ea7525ba47caaa07fd408b84
Le Vivilet <levivlet@pm.me>
2024-04-06 16:11:22 +0200

    feature: provide real diagnostics

--- packages/typescript-worker/src/parts/Diagnostics/Diagnostics.ts
+++ packages/typescript-worker/src/parts/Diagnostics/Diagnostics.ts
@@ -1,18 +1,12 @@
 import type { Diagnostic } from '../Diagnostic/Diagnostic.ts'
 import * as TypeScriptRpc from '../TypeScriptRpc/TypeScriptRpc.ts'
+import * as GetDiagnosticFromTsResult from '../GetDiagnosticFromTsResult/GetDiagnosticFromTsResult.ts'
 
 export const getDiagnostics = async (textDocument): Promise<readonly Diagnostic[]> => {
   await TypeScriptRpc.invoke('UpdateOpen.updateOpen', [textDocument])
   const tsResult = await TypeScriptRpc.invoke('Diagnostic.getDiagnostics', {
     file: textDocument.uri,
   })
-  console.log({ tsResult })
-  return [
-    {
-      uri: textDocument.uri,
-      rowIndex: 0,
-      columnIndex: 0,
-      message: 'test error 1',
-    },
-  ]
+  const diagnostics = GetDiagnosticFromTsResult.getDiagnosticsFromTsResult(textDocument, tsResult)
+  return diagnostics
 }
