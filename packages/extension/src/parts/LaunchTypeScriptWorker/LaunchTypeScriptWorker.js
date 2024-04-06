926e8bfabf4d751da9c56e3fb28bd730ef9bbc3a
Le Vivilet <levivlet@pm.me>
2024-04-06 15:59:05 +0200

    feature: add more typescript worker rpc functions

--- packages/extension/src/parts/LaunchTypeScriptWorker/LaunchTypeScriptWorker.js
+++ packages/extension/src/parts/LaunchTypeScriptWorker/LaunchTypeScriptWorker.js
@@ -1,10 +1,14 @@
+import * as GetTsServerPath from '../GetTsServerPath/GetTsServerPath.js'
 import * as LaunchWorker from '../LaunchWorker/LaunchWorker.js'
 import * as TypeScriptWorkerUrl from '../TypeScriptWorkerUrl/TypeScriptWorkerUrl.js'
 
-export const launchTypeScriptWorker = () => {
-  return LaunchWorker.launchWorker({
+export const launchTypeScriptWorker = async () => {
+  const worker = await LaunchWorker.launchWorker({
     url: TypeScriptWorkerUrl.typeScriptWorkerUrl,
     name: 'Prettier Worker',
     contentSecurityPolicy: "default-src 'none'; script-src 'self'",
   })
+  const typeScriptPath = await GetTsServerPath.getTsServerPath()
+  await worker.invoke('Initialize.initialize', typeScriptPath)
+  return worker
 }
--- packages/typescript-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/typescript-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as Diagnostics from '../Diagnostics/Diagnostics.ts'
+import * as Initialize from '../Initialize/Initialize.ts'
 
 export const commandMap = {
   'Diagnostic.getDiagnostics': Diagnostics.getDiagnostics,
+  'Initialize.initialize': Initialize.initialize,
 }
