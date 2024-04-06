6b05b0f0421530acadaff98b7eef5ef30bbca579
Le Vivilet <levivlet@pm.me>
2024-04-06 20:31:38 +0200

    feature: add diagnostic tests

--- packages/typescript-worker/src/parts/GetDiagnosticFromTsResult/GetDiagnosticFromTsResult.ts
+++ packages/typescript-worker/src/parts/GetDiagnosticFromTsResult/GetDiagnosticFromTsResult.ts
@@ -1,4 +1,5 @@
 import * as IsStyleDiagnostic from '../IsStyleDiagnostic/IsStyleDiagnostic.ts'
+import type * as TypeScriptProtocol from '../TypeScriptProtocol/TypeScriptProtocol.cts'
 
 const getSeverity = (diagnostic) => {
   if (IsStyleDiagnostic.isStyleCheckDiagnostic(diagnostic.code) && diagnostic.category === 'error') {
@@ -6,10 +7,11 @@ const getSeverity = (diagnostic) => {
   }
   return 'error'
 }
+
 /**
  *
  */
-const convertTsDiagnostic = (textDocument, diagnostic) => {
+const convertTsDiagnostic = (textDocument: any, diagnostic: TypeScriptProtocol.Diagnostic) => {
   return {
     rowIndex: diagnostic.start.line - 1,
     columnIndex: diagnostic.start.offset - 1, // TODO should be offset based here
@@ -24,9 +26,13 @@ const convertTsDiagnostic = (textDocument, diagnostic) => {
   }
 }
 
-export const getDiagnosticsFromTsResult = (textDocument, tsResult) => {
+export const getDiagnosticsFromTsResult = (
+  textDocument: any,
+  tsResult: TypeScriptProtocol.SemanticDiagnosticsSyncResponse['body'],
+) => {
   const diagnostics = []
   for (const tsDiagnostic of tsResult) {
+    // @ts-ignore
     diagnostics.push(convertTsDiagnostic(textDocument, tsDiagnostic))
   }
   return diagnostics
---
+++ packages/typescript-worker/test/GetDiagnosticFromTsResult.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@jest/globals'
+import * as GetDiagnosticFromTsResult from '../src/parts/GetDiagnosticFromTsResult/GetDiagnosticFromTsResult.ts'
+import type * as TypeScriptProtocol from '../src/parts/TypeScriptProtocol/TypeScriptProtocol.cts'
+
+test('empty string', () => {
+  const textDocument = {
+    uri: '/test/index.ts',
+  }
+  const tsResult: TypeScriptProtocol.Diagnostic[] = [
+    {
+      category: 'error',
+      end: {
+        line: 1,
+        offset: 1,
+      },
+      start: {
+        line: 1,
+        offset: 1,
+      },
+      text: 'test text',
+    },
+  ]
+  expect(GetDiagnosticFromTsResult.getDiagnosticsFromTsResult(textDocument, tsResult)).toEqual([
+    {
+      code: undefined,
+      columnIndex: 0,
+      endColumnIndex: 0,
+      endRowIndex: 0,
+      message: 'test text',
+      rowIndex: 0,
+      source: 'ts',
+      type: 'error',
+      uri: '/test/index.ts',
+    },
+  ])
+})
