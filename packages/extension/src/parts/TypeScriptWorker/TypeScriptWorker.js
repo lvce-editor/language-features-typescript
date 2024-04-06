41f74ed6f20897c63387f21e27963c227f89346a
Le Vivilet <levivlet@pm.me>
2024-04-06 15:37:23 +0200

    feature: add function to get or create typescript worker

---
+++ packages/extension/src/parts/TypeScriptWorker/TypeScriptWorker.js
@@ -0,0 +1,18 @@
+import * as LaunchTypeScriptWorker from '../LaunchTypeScriptWorker/LaunchTypeScriptWorker.js'
+
+export const state = {
+  ipc: undefined,
+  rpcPromise: undefined,
+}
+
+const getOrCreateRpc = async () => {
+  if (!state.rpcPromise) {
+    state.rpcPromise = LaunchTypeScriptWorker.launchTypeScriptWorker()
+  }
+  return state.rpcPromise
+}
+
+export const getInstance = async () => {
+  const rpc = await getOrCreateRpc()
+  return rpc
+}
