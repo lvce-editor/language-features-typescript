1b9f9c94ec866b25d3ff1cbeb9769f04bfbd6363
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-06 19:16:56 +0200

    Discard changes to packages/extension/src/parts/ExtensionHost/ExtensionHostCompletionProviderTypeScript.js

--- packages/extension/src/parts/ExtensionHost/ExtensionHostCompletionProviderTypeScript.js
+++ packages/extension/src/parts/ExtensionHost/ExtensionHostCompletionProviderTypeScript.js
@@ -1,17 +1,23 @@
-import * as TypeScriptWorker from '../TypeScriptWorker/TypeScriptWorker.js'
+// @ts-nocheck
+import * as Completion from '../Completion/Completion.js'
+import * as GetCompletionFromTsResult from '../GetCompletionFromTsResult/GetCompletionFromTsResult.js'
+import * as GetResolveCompletionItemFromTsResult from '../GetResolvedCompletionItemFromTsResult/GetResolvedCompletionItemFromTsResult.js'
+import * as ResolveCompletion from '../ResolveCompletion/ResolveCompletion.js'
 
 /**
- * @type {any }
+ * @type {vscode.CompletionProvider['provideCompletions']}
  */
 export const provideCompletions = async (textDocument, offset) => {
-  const worker = await TypeScriptWorker.getInstance()
-  return worker.invoke('Completion.getCompletions', textDocument, offset)
+  const tsResult = await Completion.getCompletion(textDocument, offset)
+  const items = GetCompletionFromTsResult.getCompletionFromTsResult(tsResult)
+  return items
 }
 
 // /**
-//  * @type {any}
+//  * @type {vscode.CompletionProvider['resolveCompletionItem']}
 //  */
 export const resolveCompletionItem = async (textDocument, offset, name, completionItem) => {
-  const worker = await TypeScriptWorker.getInstance()
-  return worker.invoke('Completion.resolveCompletion', textDocument, offset, name, completionItem)
+  const tsResult = await ResolveCompletion.resolveCompletion(textDocument, offset, name, completionItem)
+  const result = GetResolveCompletionItemFromTsResult.getResolveCompletionItemFromTsResult(tsResult)
+  return result
 }
