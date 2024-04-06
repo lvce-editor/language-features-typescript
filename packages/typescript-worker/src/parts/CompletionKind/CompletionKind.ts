3a7f4a6bf51d682803e750af96dd655dd5696f2f
Le Vivilet <levivlet@pm.me>
2024-04-06 18:56:49 +0200

    feature: compute completions in typescript worker

---
+++ packages/typescript-worker/src/parts/CompletionKind/CompletionKind.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+export const Field = vscode.EditorCompletionType.Field
+export const File = vscode.EditorCompletionType.File
+export const Folder = vscode.EditorCompletionType.Folder
+export const Function = vscode.EditorCompletionType.Function
+export const Keyword = vscode.EditorCompletionType.Keyword
+export const Property = vscode.EditorCompletionType.Property
+export const Value = vscode.EditorCompletionType.Value
+export const Variable = vscode.EditorCompletionType.Variable
+export const Unknown = 0
---
+++ packages/typescript-worker/src/parts/ConvertTsCompletionEntry/ConvertTsCompletionEntry.ts
@@ -0,0 +1,21 @@
+import * as ConvertCompletionItemKind from '../ConvertCompletionItemKind/ConvertCompletionItemKind.ts'
+import * as ConvertTsCompletionKindModifiers from '../ConvertTsCompletionKindModifiers/ConvertTsCompletionKindModifiers.js'
+
+// TODO don't pass lots of data to renderer worker
+// source and data properties are only necessary for resolveCompletionItem
+
+/**
+ *
+ * @param {import('../TsServerProtocol/TsServerProtocol.js').CompletionEntry} tsEntry
+ */
+export const convertTsCompletionEntry = (tsEntry) => {
+  const { name, kind, source, data, kindModifiers } = tsEntry
+  return {
+    label: name,
+    snippet: name,
+    kind: ConvertCompletionItemKind.convertCompletionItemKind(kind),
+    source,
+    data,
+    flags: ConvertTsCompletionKindModifiers.convertTsCompletionKindModifiers(kindModifiers),
+  }
+}
