631f4ba26b24f0798e73fca9bf66dc664724f7cf
Le Vivilet <levivlet@pm.me>
2024-04-07 15:29:01 +0200

    feature: add integration test

---
+++ packages/integration/test/initialize.test.ts
@@ -0,0 +1,16 @@
+import { testWorker } from '../src/testWorker.js'
+import { test, expect } from '@jest/globals'
+
+test('completion', async () => {
+  const execMap = {}
+  const worker = await testWorker({
+    execMap,
+  })
+  const uri = '/test/file.html'
+  const content = ' <h1>hello world</h1>'
+  expect(await worker.execute('Prettier.format', uri, content)).toEqual({
+    startOffset: 0,
+    endOffset: 21,
+    inserted: '<h1>hello world</h1>\n',
+  })
+})
