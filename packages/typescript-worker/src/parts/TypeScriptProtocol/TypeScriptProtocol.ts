03eeb70d05762b15934cd41419c171341ddc84aa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-07 09:56:08 +0200

    feature: add definition to typescript worker (#250)

--- packages/typescript-worker/src/parts/TypeScriptProtocol/TypeScriptProtocol.ts
+++ packages/typescript-worker/src/parts/TypeScriptProtocol/TypeScriptProtocol.ts
@@ -1,6 +1,7 @@
 import type * as ts from 'typescript/lib/tsserverlibrary.js'
 
 export type CompletionEntry = ts.server.protocol.CompletionEntry
+export type DefinitionInfo = ts.server.protocol.DefinitionInfo
 export type Diagnostic = ts.server.protocol.Diagnostic
 export type QuickInfoResponseBody = ts.server.protocol.QuickInfoResponseBody
 export type SemanticDiagnosticsSyncResponse = ts.server.protocol.SemanticDiagnosticsSyncResponse
