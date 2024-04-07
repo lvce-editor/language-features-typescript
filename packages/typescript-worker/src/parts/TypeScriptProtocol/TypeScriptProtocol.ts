21ae1327aa829d7c2402b4d2069e1c40c8add792
Le Vivilet <levivlet@pm.me>
2024-04-07 09:37:18 +0200

    add test

--- packages/typescript-worker/src/parts/TypeScriptProtocol/TypeScriptProtocol.ts
+++ packages/typescript-worker/src/parts/TypeScriptProtocol/TypeScriptProtocol.ts
@@ -4,3 +4,4 @@ export type CompletionEntry = ts.server.protocol.CompletionEntry
 export type Diagnostic = ts.server.protocol.Diagnostic
 export type QuickInfoResponseBody = ts.server.protocol.QuickInfoResponseBody
 export type SemanticDiagnosticsSyncResponse = ts.server.protocol.SemanticDiagnosticsSyncResponse
+export type ReferencesResponseBody = ts.server.protocol.ReferencesResponseBody
