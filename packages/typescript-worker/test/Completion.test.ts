1e00996dd48946b1d55cfaca75fcc28684221fa5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-07 11:55:48 +0200

    feature: simplify completion test (#255)

--- packages/typescript-worker/test/Completion.test.ts
+++ packages/typescript-worker/test/Completion.test.ts
@@ -1,43 +1,26 @@
-import { beforeEach, expect, jest, test } from '@jest/globals'
-
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('../src/parts/TypeScriptRpc/TypeScriptRpc.ts', () => {
-  return {
-    invoke: jest.fn(),
-  }
-})
-jest.unstable_mockModule('../src/parts/Rpc/Rpc.ts', () => {
-  return {
-    invoke: jest.fn(),
-  }
-})
-
-const Completion = await import('../src/parts/Completion/Completion.ts')
-const TypeScriptRpc = await import('../src/parts/TypeScriptRpc/TypeScriptRpc.ts')
-const Rpc = await import('../src/parts/Rpc/Rpc.ts')
+import { expect, jest, test } from '@jest/globals'
+import type { CommonRpc } from '../src/parts/CommonRpc/CommonRpc.ts'
+import * as Completion from '../src/parts/Completion/Completion.ts'
 
 test('getCompletion', async () => {
-  jest.spyOn(TypeScriptRpc, 'invoke').mockImplementation(async (method) => {
-    if (method === 'Completion.getCompletion') {
+  const typeScriptRpc: CommonRpc = {
+    invoke: jest.fn(async () => {
       return {
         entries: [],
-      }
-    }
-  })
-  jest.spyOn(Rpc, 'invoke').mockImplementation(async (method) => {
-    if (method === 'Position.getPosition') {
+      } as any
+    }),
+  }
+  const Position = {
+    getTsPosition() {
       return {
-        rowIndex: 0,
-        columnIndex: 0,
+        line: 0,
+        column: 0,
       }
-    }
-  })
+    },
+  }
   const textDocument = {
     uri: '',
   }
   const offset = 0
-  expect(await Completion.getCompletion(textDocument, offset)).toEqual([])
+  expect(await Completion.getCompletion(typeScriptRpc, Position, textDocument, offset)).toEqual([])
 })
