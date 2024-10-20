9e11c35fb04e5a5c41a005f8cfa59486a0e67f83
Le Vivilet <levivlet@pm.me>
2024-10-20 16:27:30 +0200

    move filesystem

---
+++ packages/typescript-worker/src/parts/FileSystem/FileSystem.ts
@@ -0,0 +1,55 @@
+import * as TypeScriptUrl from '../TypeScriptUrl/TypeScriptUrl.ts'
+
+const getTextSync = (url) => {
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', /* url */ url, /* async */ false)
+  xhr.send(null)
+  return xhr.responseText
+}
+
+const files = Object.create(null)
+
+let version = 0
+
+export const getVersion = () => {
+  return version
+}
+
+export const getAllFiles = () => {
+  return Object.keys(files)
+}
+export const writeFile = (path: string, content: string) => {
+  version++
+  const existing = files[path]
+  if (existing) {
+    existing.text = content
+    existing.version++
+  } else {
+    files[path] = {
+      text: content,
+      version: 0,
+    }
+  }
+}
+
+export const readFile = (path: string) => {
+  const file = files[path]
+  if (!file) {
+    if (path.startsWith('/lib')) {
+      path = `${TypeScriptUrl.typescriptBaseUrl}/lib${path}`
+      const text = getTextSync(path) // typescript only supports synchronous file system
+      return text
+    }
+    console.info(`[info] file ${path} not found`)
+    return ''
+  }
+  return file.text
+}
+
+export const readVersion = (path: string) => {
+  const file = files[path]
+  if (!file) {
+    return '0'
+  }
+  return file.version
+}
--- packages/typescript-worker/src/parts/TypeScriptLanguageService/TypeScriptLanguageService.ts
+++ packages/typescript-worker/src/parts/TypeScriptLanguageService/TypeScriptLanguageService.ts
@@ -1,57 +1,9 @@
 import type { LanguageServiceHost } from 'typescript'
+import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as LoadTypeScript from '../LoadTypeScript/LoadTypeScript.ts'
 import * as TypeScriptUrl from '../TypeScriptUrl/TypeScriptUrl.ts'
 
 const ts = await LoadTypeScript.loadTypeScript(TypeScriptUrl.typeScriptUrl)
-
-const getTextSync = (url) => {
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET', /* url */ url, /* async */ false)
-  xhr.send(null)
-  return xhr.responseText
-}
-
-const fileSystem = {
-  version: 0,
-  // "/index.ts": "",
-  files: Object.create(null),
-  getAllFiles() {
-    return Object.keys(this.files)
-  },
-  writeFile(path, content) {
-    this.version++
-    const existing = this.files[path]
-    if (existing) {
-      existing.text = content
-      existing.version++
-    } else {
-      this.files[path] = {
-        text: content,
-        version: 0,
-      }
-    }
-  },
-  readFile(path) {
-    const file = this.files[path]
-    if (!file) {
-      if (path.startsWith('/lib')) {
-        path = `${TypeScriptUrl.typescriptBaseUrl}/lib${path}`
-        const text = getTextSync(path) // typescript only supports synchronous file system
-        return text
-      }
-      console.info(`[info] file ${path} not found`)
-      return ''
-    }
-    return file.text
-  },
-  readVersion(path) {
-    const file = this.files[path]
-    if (!file) {
-      return '0'
-    }
-    return file.version
-  },
-}
 
 const languageServiceHost: LanguageServiceHost = {
   readFile(path) {
@@ -70,13 +22,13 @@ const languageServiceHost: LanguageServiceHost = {
     return false
   },
   getProjectVersion() {
-    return `${fileSystem.version}`
+    return `${FileSystem.getVersion()}`
   },
   getScriptFileNames() {
-    return fileSystem.getAllFiles()
+    return FileSystem.getAllFiles()
   },
   getScriptVersion(fileName) {
-    return fileSystem.readVersion(fileName)
+    return FileSystem.readVersion(fileName)
   },
   writeFile(fileName, content) {
     throw new Error('not implemented')
@@ -94,7 +46,7 @@ const languageServiceHost: LanguageServiceHost = {
     return '/' + ts.getDefaultLibFileName(options)
   },
   getScriptSnapshot(fileName) {
-    const content = fileSystem.readFile(fileName)
+    const content = FileSystem.readFile(fileName)
     const snapshot = ts.ScriptSnapshot.fromString(content)
     return snapshot
   },
@@ -102,4 +54,4 @@ const languageServiceHost: LanguageServiceHost = {
 
 const languageService = ts.createLanguageService(languageServiceHost, undefined, false)
 
-export { fileSystem, languageService }
+export { languageService }
