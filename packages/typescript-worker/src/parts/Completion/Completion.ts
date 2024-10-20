2a585d0c965cdd1118327bb41772e800f5701be3
Le Vivilet <levivlet@pm.me>
2024-10-20 17:47:35 +0200

    remove unused code

--- packages/typescript-worker/src/parts/Completion/Completion.ts
+++ packages/typescript-worker/src/parts/Completion/Completion.ts
@@ -14,22 +14,6 @@ export const getCompletion = async (typeScriptRpc: CommonRpc, Position: any, tex
   const completions1 = languageService.getCompletionsAtPosition(uri, offset, {})
 
   completions1.entries
-  // console.log({ completions1 })
-  // console.log({ languageService })
-  // const typescript = await LoadTypeScript.loadTypeScript(TypeScriptUrl.typeScriptUrl)
-
-  // console.log({ typescript })
-  // console.log('before completion', uri)
-  // await TextDocumentSync.openTextDocuments2(typeScriptRpc, [textDocument])
-  // const tsPosition = await Position.getTsPosition(textDocument, offset)
-  // const tsResult = await typeScriptRpc.invoke<TypeScriptProtocol.CompletionInfoResponse['body']>(
-  //   'Completion.getCompletion',
-  //   {
-  //     file: textDocument.uri,
-  //     line: tsPosition.line,
-  //     offset: tsPosition.offset,
-  //   },
-  // )
   const completions = GetCompletionFromTsResult.getCompletionFromTsResult(completions1)
   return completions
 }
