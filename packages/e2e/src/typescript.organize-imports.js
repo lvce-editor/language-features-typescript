4946e98f9163eb3deba7d66293c56aea4d0a31e7
Le Vivilet <levivlet@pm.me>
2024-10-20 14:18:04 +0200

    feature: add organizeImports e2e test

---
+++ packages/e2e/src/typescript.organize-imports.js
@@ -0,0 +1,36 @@
+export const name = 'typescript.organize-imports'
+
+export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, QuickPick }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/a.ts`, 'export const a = 1')
+  await FileSystem.writeFile(`${tmpDir}/b.ts`, 'export const b = 2')
+  await FileSystem.writeFile(
+    `${tmpDir}/c.ts`,
+    `import { a } from './a'
+import { b } from './b'
+
+export const c = a + 1`,
+  )
+
+  await Main.openUri(`${tmpDir}/c.ts`)
+  // await Editor.setCursor(0, 3)
+
+  // act
+  await QuickPick.open()
+  const quickPick = Locator('#QuickPick')
+  await expect(quickPick).toBeVisible()
+  const quickPickInput = quickPick.locator('input')
+  await expect(quickPickInput).toHaveValue('>')
+  await new Promise((r) => {
+    setTimeout(r, 1000)
+  })
+  await QuickPick.setValue('> Organize Imports')
+  // await Editor.openCompletion()
+
+  // assert
+  console.log({ Editor, QuickPick })
+  // await expect(completions).toBeVisible()
+  // const completionItems = completions.locator('.EditorCompletionItem')
+  // await expect(completionItems.nth(0)).toHaveText('PictureInPictureWindow')
+}
