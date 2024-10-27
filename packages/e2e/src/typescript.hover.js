054473477d2b354834249e42e8d5d88ef8116508
Le Vivilet <levivlet@pm.me>
2024-10-27 20:12:39 +0100

    feature: add hover e2e test

---
+++ packages/e2e/src/typescript.hover.js
@@ -0,0 +1,22 @@
+export const name = 'typescript.completion'
+
+export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/test.ts`,
+    `let x = 1
+
+export {}
+`,
+  )
+  await Main.openUri(`${tmpDir}/test.ts`)
+  await Editor.setCursor(0, 4)
+
+  // act
+  await Editor.openHover()
+
+  // assert
+  const hover = Locator('.HoverDisplayString')
+  await expect(hover).toHaveText('let x: number')
+}
