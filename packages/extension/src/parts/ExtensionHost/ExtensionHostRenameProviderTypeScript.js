71cc396a1fd5d28c5ce0b069146779e9fc516e79
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-29 21:56:33 +0100

    feature: register rename provider in typescript node process (#305)

--- packages/extension/src/parts/ExtensionHost/ExtensionHostRenameProviderTypeScript.js
+++ packages/extension/src/parts/ExtensionHost/ExtensionHostRenameProviderTypeScript.js
@@ -1,4 +1,3 @@
-import * as Position from '../Position/Position.js'
 import * as LanguageId from '../LanguageId/LanguageId.js'
 import * as TypeScriptWorker from '../TypeScriptWorker/TypeScriptWorker.js'
 
@@ -10,35 +9,7 @@ export const prepareRename = async (textDocument, offset) => {
   //  TODO
 }
 
-export const getRenameResultFromTsResult = (textDocument, tsResult, newName) => {
-  if (!tsResult.info.canRename) {
-    // TODO how to handle this kind of error vs programmer error?
-    throw new Error('rename was not successful')
-  }
-  const workspaceEdits = []
-  for (const spanGroup of tsResult.locs) {
-    const edits = []
-    for (const textSpan of spanGroup.locs) {
-      const prefixText = textSpan.prefixText || ''
-      const suffixText = textSpan.suffixText || ''
-      const inserted = prefixText + newName + suffixText
-      const offset = Position.getOffset(textDocument, textSpan.start)
-      console.log({ offset })
-      edits.push({
-        offset,
-        inserted,
-        deleted: 0,
-      })
-    }
-    workspaceEdits.push({
-      file: spanGroup.file,
-      edits,
-    })
-  }
-  return workspaceEdits
-}
-
-export const rename = async (textDocument, offset, newName) => {
+export const provideRename = async (textDocument, offset, newName) => {
   const worker = await TypeScriptWorker.getInstance()
   return worker.invoke('Rename.rename', textDocument, offset, newName)
 }
