9570fd7e3695fe238911011a0db7c72ad53f5fea
Le Vivilet <levivlet@pm.me>
2024-10-31 11:15:26 +0100

    feature: format e2e tests

--- packages/e2e/src/typescript.semantic-highlighting.js
+++ packages/e2e/src/typescript.semantic-highlighting.js
@@ -2,27 +2,20 @@ export const name = 'typescript.semantic-highlighting'
 
 export const skip = true
 
-export const test = async ({
-  FileSystem,
-  Workspace,
-  Main,
-  Editor,
-  Locator,
-  expect,
-}) => {
+export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(
     `${tmpDir}/add.js`,
     `export const add = () => {}
-`
+`,
   )
   await FileSystem.writeFile(
     `${tmpDir}/test.js`,
     `import {add} from './add.js'
 
 add(1,2)
-`
+`,
   )
   await FileSystem.writeFile(`${tmpDir}/tsconfig.json`, `{}`)
 
