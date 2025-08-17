70533bbd4216d761a957483daebc705301c3dbc1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-17 22:27:09 +0200

    feature: add hover2 (#354)

---
+++ packages/typescript-worker/src/parts/Hover2/Hover2.ts
@@ -0,0 +1,15 @@
+import { createTypeScriptLanguageService } from '../CreateTypeScriptLanguageService/CreateTypeScriptLanguageService.ts'
+import { getTypeScriptPath } from '../GetTypeScriptPath/GetTypeScriptPath.ts'
+import { loadTypeScript } from '../LoadTypeScript/LoadTypeScript.ts'
+
+export const getHover2 = async (textDocument: any, offset: number) => {
+  const tsPath = getTypeScriptPath()
+  console.time('load')
+  const ts = await loadTypeScript(tsPath)
+  const languageService = createTypeScriptLanguageService(ts)
+  console.timeEnd('load')
+  console.time('result')
+  const result = languageService.getQuickInfoAtPosition(textDocument.uri, offset)
+  console.timeEnd('result')
+  return result
+}
