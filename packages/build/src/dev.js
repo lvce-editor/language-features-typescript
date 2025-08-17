005dfb45af02c128395a1c095874ff35eb524699
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-17 16:06:27 +0200

    feature: update dev script (#349)

--- packages/build/src/dev.js
+++ packages/build/src/dev.js
@@ -33,10 +33,18 @@ const main = async () => {
       stdio: 'inherit',
     },
   )
-  execa('node', ['packages/server/node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e'], {
-    cwd: root,
-    stdio: 'inherit',
-  })
+  execa(
+    'node',
+    [
+      'packages/server/node_modules/@lvce-editor/server/bin/server.js',
+      '--test-path=packages/e2e',
+      '--only-extension=packages/extension',
+    ],
+    {
+      cwd: root,
+      stdio: 'inherit',
+    },
+  )
 }
 
 main()
