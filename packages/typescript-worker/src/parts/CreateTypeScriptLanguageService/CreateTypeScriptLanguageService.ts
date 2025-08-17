44bbfde09c749a416fd69870ace8c9b82b6ad2c5
Le Vivilet <levivlet@pm.me>
2025-08-17 22:35:06 +0200

    feature: use basic file system

--- packages/typescript-worker/src/parts/CreateTypeScriptLanguageService/CreateTypeScriptLanguageService.ts
+++ packages/typescript-worker/src/parts/CreateTypeScriptLanguageService/CreateTypeScriptLanguageService.ts
@@ -1,8 +1,9 @@
 import type { LanguageService, LanguageServiceHost } from 'typescript'
+import { type IFileSystem } from '../CreateFileSystem/CreateFileSystem.ts'
 import * as TypeScriptLanguageHost from '../TypeScriptLanguageHost/TypeScriptLanguageHost.ts'
 
-export const createTypeScriptLanguageService = (ts: typeof import('typescript')): LanguageService => {
-  const languageServiceHost: LanguageServiceHost = TypeScriptLanguageHost.create(ts)
+export const createTypeScriptLanguageService = (ts: typeof import('typescript'), fs: IFileSystem): LanguageService => {
+  const languageServiceHost: LanguageServiceHost = TypeScriptLanguageHost.create(ts, fs)
   const languageService = ts.createLanguageService(languageServiceHost)
   return languageService
 }
