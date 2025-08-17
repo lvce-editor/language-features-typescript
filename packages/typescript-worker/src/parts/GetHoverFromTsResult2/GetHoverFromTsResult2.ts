366acedbe51be856eb2c396daa6e20cdc2c0148b
Le Vivilet <levivlet@pm.me>
2025-08-17 22:42:14 +0200

    feature: use new hover provider, using typescript from webworker

---
+++ packages/typescript-worker/src/parts/GetHoverFromTsResult2/GetHoverFromTsResult2.ts
@@ -0,0 +1,15 @@
+import type ts from 'typescript'
+
+export const getHoverFromTsResult2 = (tsResult: ts.QuickInfo) => {
+  if (!tsResult) {
+    return undefined
+  }
+  const displayStringParts = tsResult.displayParts || []
+  const displayString = displayStringParts.map((item) => item.text).join('')
+  tsResult.textSpan
+  return {
+    displayString: displayString,
+    documentation: tsResult.documentation,
+    languageId: 'typescript',
+  }
+}
--- packages/typescript-worker/src/parts/Hover/Hover.ts
+++ packages/typescript-worker/src/parts/Hover/Hover.ts
@@ -1,20 +1,11 @@
 import * as Assert from '../Assert/Assert.ts'
 import type { CommonRpc } from '../CommonRpc/CommonRpc.ts'
-import * as GetHoverFromTsResult from '../GetHoverFromTsResult/GetHoverFromTsResult.ts'
-import * as TextDocumentSync from '../TextDocumentSync/TextDocumentSync.ts'
-import type * as TypeScriptProtocol from '../TypeScriptProtocol/TypeScriptProtocol.ts'
+import { getHover2 } from '../Hover2/Hover2.ts'
 
 export const getHover = async (typescriptRpc: CommonRpc, Position: any, textDocument: any, offset: number) => {
   const uri = textDocument.uri
   Assert.string(uri)
   Assert.number(offset)
-  await TextDocumentSync.openTextDocuments2(typescriptRpc, [textDocument])
-  const tsPosition = await Position.getTsPosition(textDocument, offset)
-  const tsResult = await typescriptRpc.invoke<TypeScriptProtocol.QuickInfoResponseBody>('Hover.getHover', {
-    file: textDocument.uri,
-    line: tsPosition.line,
-    offset: tsPosition.offset,
-  })
-  const hover = GetHoverFromTsResult.getHoverFromTsResult(tsResult)
-  return hover
+  const result = await getHover2(textDocument, offset)
+  return result
 }
