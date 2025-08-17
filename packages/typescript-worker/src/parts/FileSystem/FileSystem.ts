846061587df73977e5f60dbd2f5710e0361976f4
Le Vivilet <levivlet@pm.me>
2025-08-17 22:03:30 +0200

    feature: add typescript webworker functions

---
+++ packages/typescript-worker/src/parts/FileSystem/FileSystem.ts
@@ -0,0 +1,31 @@
+import * as IsLibFile from '../IsLibFile/IsLibFile.ts'
+import * as ReadLibFile from '../ReadLibFile/ReadLibFile.ts'
+
+export const state = {
+  files: Object.create(null),
+  version: 1,
+}
+
+export const readFile = (uri) => {
+  if (IsLibFile.isLibFile(uri)) {
+    return ReadLibFile.readLibFile(uri)
+  }
+  return state.files[uri]
+}
+
+export const writeFile = (uri, text) => {
+  state.files[uri] = text
+  state.version++
+}
+
+export const getVersion = () => {
+  return state.version
+}
+
+export const getAllFiles = () => {
+  return Object.keys(state.files)
+}
+
+export const readVersion = (fileName) => {
+  return '1'
+}
---
+++ packages/typescript-worker/src/parts/ReadLibFile/ReadLibFile.ts
@@ -0,0 +1,10 @@
+import * as GetTextSync from '../GetTextSync/GetTextSync.ts'
+import * as GetLibFileUrl from '../GetLibFileUrl/GetLibFileUrl.ts'
+
+export const readLibFile = (uri) => {
+  const url = GetLibFileUrl.getLibFileUrl(uri)
+  if (!url) {
+    return undefined
+  }
+  return GetTextSync.getTextSync(uri)
+}
