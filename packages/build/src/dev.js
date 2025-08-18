0c54b29bc17cb030e1232e56a8dc2e41189f55e2
Le Vivilet <levivlet@pm.me>
2025-08-18 13:22:47 +0200

    feature: add build watch script

--- packages/build/src/dev.js
+++ packages/build/src/dev.js
@@ -1,38 +1,9 @@
 import { execa } from 'execa'
 import { root } from './root.js'
-import { join } from 'node:path'
 
 const main = async () => {
-  const binaryName = process.platform === 'win32' ? 'esbuild.exe' : 'esbuild'
-  const esbuildPath = join(root, 'packages', 'build', 'node_modules', 'esbuild', 'bin', binaryName)
-  execa(
-    esbuildPath,
-    [
-      '--format=esm',
-      '--bundle',
-      '--watch',
-      'packages/extension/src/languageFeaturesTypeScriptMain.js',
-      '--outfile=packages/extension/dist/languageFeaturesTypeScriptMain.js',
-    ],
-    {
-      cwd: root,
-      stdio: 'inherit',
-    },
-  )
-  execa(
-    esbuildPath,
-    [
-      '--format=esm',
-      '--bundle',
-      '--watch',
-      'packages/typescript-worker/src/typescriptWorkerMain.ts',
-      '--outfile=packages/typescript-worker/dist/typescriptWorkerMain.js',
-    ],
-    {
-      cwd: root,
-      stdio: 'inherit',
-    },
-  )
+  await import('./build-watch.js')
+
   execa(
     'node',
     [
