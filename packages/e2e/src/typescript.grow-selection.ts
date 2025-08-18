0ddf17808411ba99e51de5b8c57aad9044f7653a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-18 12:30:14 +0200

    feature: add grow selection e2e test (#367)

---
+++ packages/e2e/src/typescript.grow-selection.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'typescript.grow-selection'
+
+export const skip = 1
+
+export const test: Test = async ({ FileSystem, Main, Editor, Command, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.ts`, 'let x = 1')
+  await Main.openUri(`${tmpDir}/test.ts`)
+  await Editor.setCursor(0, 3)
+
+  // act
+  await Command.execute('Editor.selectionGrow')
+
+  // assert
+  // TODO
+}
