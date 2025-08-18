a8ccb0d8bd307d3af1eea3420d23a2087c1608e6
Le Vivilet <levivlet@pm.me>
2025-08-18 09:15:24 +0200

    feature: add language services

--- packages/typescript-worker/src/parts/Hover2/Hover2.ts
+++ packages/typescript-worker/src/parts/Hover2/Hover2.ts
@@ -1,14 +1,9 @@
-import { createFileSystem } from '../CreateFileSystem/CreateFileSystem.ts'
-import { createTypeScriptLanguageService } from '../CreateTypeScriptLanguageService/CreateTypeScriptLanguageService.ts'
 import { getHoverFromTsResult2 } from '../GetHoverFromTsResult2/GetHoverFromTsResult2.ts'
-import { getTypeScriptPath } from '../GetTypeScriptPath/GetTypeScriptPath.ts'
-import { loadTypeScript } from '../LoadTypeScript/LoadTypeScript.ts'
+import * as LanguageServices from '../LanguageServices/LanguageServices.ts'
 
 export const getHover2 = async (textDocument: any, offset: number) => {
-  const tsPath = getTypeScriptPath()
-  const ts = await loadTypeScript(tsPath)
-  const fs = createFileSystem()
-  const languageService = createTypeScriptLanguageService(ts, fs)
+  const id = 1
+  const { languageService, fs } = LanguageServices.get(id)
   fs.writeFile(textDocument.uri, textDocument.text)
   const tsResult = languageService.getQuickInfoAtPosition(textDocument.uri, offset)
   const hover = getHoverFromTsResult2(tsResult)
--- packages/typescript-worker/src/parts/Initialize/Initialize.ts
+++ packages/typescript-worker/src/parts/Initialize/Initialize.ts
@@ -1,3 +1,16 @@
+import { createFileSystem } from '../CreateFileSystem/CreateFileSystem.ts'
+import { createTypeScriptLanguageService } from '../CreateTypeScriptLanguageService/CreateTypeScriptLanguageService.ts'
+import { getTypeScriptPath } from '../GetTypeScriptPath/GetTypeScriptPath.ts'
+import * as LanguageServices from '../LanguageServices/LanguageServices.ts'
+import { loadTypeScript } from '../LoadTypeScript/LoadTypeScript.ts'
+
 export const initialize = async (path: string) => {
-  // await TypeScriptRpc.listen(path)
+  const tsPath = getTypeScriptPath()
+  console.time('load')
+  const ts = await loadTypeScript(tsPath)
+  console.timeEnd('load')
+  const fs = createFileSystem()
+  const languageService = createTypeScriptLanguageService(ts, fs)
+  const id = 1
+  LanguageServices.set(id, languageService, fs)
 }
