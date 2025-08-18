04730ab059e531b78d3c434177adc16e77a730b5
Le Vivilet <levivlet@pm.me>
2025-08-18 11:30:44 +0200

    feature: add no reference found e2e test

---
+++ packages/e2e/src/typescript.no-reference-found.ts
@@ -0,0 +1,25 @@
+export const name = 'typescript.no-reference-found'
+
+export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/test.ts`,
+    `
+
+add(1,2)
+`,
+  )
+  await FileSystem.writeFile(`${tmpDir}/tsconfig.json`, `{}`)
+  await Main.openUri(`${tmpDir}/test.ts`)
+  await Editor.setCursor(2, 2)
+
+  // act
+  await Editor.findAllReferences()
+
+  // assert
+  const viewletLocations = Locator('.Locations')
+  await expect(viewletLocations).toBeVisible()
+  const viewletReferencesMessage = Locator('.LocationsMessage')
+  await expect(viewletReferencesMessage).toHaveText('No Results')
+}
