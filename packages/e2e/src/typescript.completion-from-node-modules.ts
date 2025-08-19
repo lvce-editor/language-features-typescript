cd5a1b5b89c2be47a4a2b0e5ec844f3d9d59725c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-19 11:19:35 +0200

    Discard changes to packages/e2e/src/typescript.completion-from-node-modules.ts

--- packages/e2e/src/typescript.completion-from-node-modules.ts
+++ packages/e2e/src/typescript.completion-from-node-modules.ts
@@ -2,13 +2,11 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.completion-from-node-modules'
 
-// export const skip = 1
+export const skip = 1
 
-export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect, Workspace }) => {
+export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
-  await FileSystem.writeFile(`${tmpDir}/node_modules/lodash/package.json`, `{ "main": "index.js", "type": "module" }`)
-  await FileSystem.writeFile(`${tmpDir}/node_modules/lodash/index.js`, `export const add = (a,b) => a + b`)
   await FileSystem.writeFile(`${tmpDir}/test.ts`, `import * as _ from ''`)
   await Main.openUri(`${tmpDir}/test.ts`)
   await Editor.setCursor(0, 20)
