4beae6994d24280784e71ca5fb3a4f76ccd3d0ed
Le Vivilet <levivlet@pm.me>
2025-08-19 09:03:29 +0200

    feature: add createSyncRpcClient

---
+++ packages/typescript-worker/src/parts/CreateSyncRpcClient/CreateSyncRpcClient.ts
@@ -0,0 +1,43 @@
+import * as Rpc from '../Rpc/Rpc.ts'
+import type { SyncRpc } from '../SyncRpc/SyncRpc.ts'
+import { waitForSyncRpcResult } from '../WaitForSyncRpcResult/WaitForSyncRpcResult.ts'
+
+export const createSyncRpcClient = async (): Promise<SyncRpc> => {
+  const syncId = 1
+  const isolated = globalThis.crossOriginIsolated
+  const buffer = isolated ? new SharedArrayBuffer(1) : undefined
+  await Rpc.invoke('SyncApi.setup', syncId, buffer)
+  const root = await navigator.storage.getDirectory()
+  const draftHandle = await root.getFileHandle('draft.txt', { create: true })
+  const resultHandle = await root.getFileHandle('result.txt', { create: true })
+  // @ts-ignore
+  const accessHandle = await draftHandle.createSyncAccessHandle({
+    mode: 'readwrite-unsafe',
+  })
+
+  // @ts-ignore
+  const resultAccessHandle = await resultHandle.createSyncAccessHandle({
+    mode: 'readwrite-unsafe',
+  })
+  return {
+    invokeSync(method, ...params) {
+      accessHandle.write(new Uint8Array([0]), { at: 0 })
+      accessHandle.truncate(1)
+      accessHandle.flush()
+      resultAccessHandle.truncate(0)
+      resultAccessHandle.flush()
+      Rpc.invoke(method, syncId, ...params)
+      const maxDelay = 1_000
+      const hasResult = waitForSyncRpcResult(accessHandle, maxDelay)
+      if (!hasResult) {
+        throw new Error(`Rpc error: timeout of ${maxDelay}ms exceeded`)
+      }
+      const size = resultAccessHandle.getSize()
+      const buf = new Uint8Array(size)
+      resultAccessHandle.read(buf)
+      const text = new TextDecoder().decode(buf)
+      const parsed = JSON.parse(text)
+      return parsed
+    },
+  }
+}
---
+++ packages/typescript-worker/src/parts/WaitForSyncRpcResult/WaitForSyncRpcResult.ts
@@ -0,0 +1,18 @@
+export const waitForSyncRpcResult = (handle: FileSystemSyncAccessHandle, maxWaitTime: number): boolean => {
+  const start = Date.now()
+  const end = start + maxWaitTime
+  let errcount = 0
+  const buffer = new Uint8Array([2])
+  let lastRead = 0
+  while (true) {
+    const now = Date.now()
+    if (now >= end) {
+      return false
+    }
+    lastRead = handle.read(buffer, { at: 0 })
+    if (buffer[0] === 1) {
+      return true
+    }
+    errcount++
+  }
+}
