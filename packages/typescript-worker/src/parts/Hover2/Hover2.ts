7877052700ea6af865ad6fdbac8a62d7ee14ec0c
Le Vivilet <levivlet@pm.me>
2025-08-19 22:42:34 +0200

    lint

--- packages/typescript-worker/src/parts/Hover2/Hover2.ts
+++ packages/typescript-worker/src/parts/Hover2/Hover2.ts
@@ -2,7 +2,6 @@ import { getHoverFromTsResult2 } from '../GetHoverFromTsResult2/GetHoverFromTsRe
 import { getOrCreateLanguageService } from '../GetOrCreateLanguageService/GetOrCreateLanguageService.ts'
 
 export const getHover2 = async (textDocument: any, offset: number) => {
-  const id = 1
   const { languageService, fs } = getOrCreateLanguageService(textDocument.uri)
   fs.writeFile(textDocument.uri, textDocument.text)
   const tsResult = languageService.getQuickInfoAtPosition(textDocument.uri, offset)
