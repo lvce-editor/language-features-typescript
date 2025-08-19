b2c31598b45e26643f99b423d1d6d782ac2c37ad
Le Vivilet <levivlet@pm.me>
2025-08-19 09:02:33 +0200

    clean up code

--- packages/typescript-worker/src/parts/CreateSyncRpcClient/CreateSyncRpcClient.ts
+++ packages/typescript-worker/src/parts/CreateSyncRpcClient/CreateSyncRpcClient.ts
@@ -10,61 +10,33 @@ export const createSyncRpcClient = async (): Promise<SyncRpc> => {
   const root = await navigator.storage.getDirectory()
   const draftHandle = await root.getFileHandle('draft.txt', { create: true })
   const resultHandle = await root.getFileHandle('result.txt', { create: true })
-
-  // f.size
-  // draftHandle.createSyncAccessHandle()
-  // Get sync access handle
-
-  // const f =await draftHandle.getFile()
-  // f.lastModified
+  // @ts-ignore
   const accessHandle = await draftHandle.createSyncAccessHandle({
     mode: 'readwrite-unsafe',
   })
 
-  const f = await draftHandle.getFile()
-  console.log({ initial: f.size })
-
+  // @ts-ignore
   const resultAccessHandle = await resultHandle.createSyncAccessHandle({
     mode: 'readwrite-unsafe',
   })
-  // accessHandle.close()
-
-  // const accessHandle2 = await draftHandle.createSyncAccessHandle({ mode: 'readwrite-unsafe' })
-  // const resultAccessHandle = await resultHandle.createSyncAccessHandle()
-
-  // Get size of the file.
-  // const fileSize = accessHandle.getSize()
-  // Read file content to a buffer.
-  // const buffer = new DataView(new ArrayBuffer(fileSize))
-  console.time('read')
-  // const readBuffer = accessHandle.read(buffer, { at: 0 })
-  console.timeEnd('read')
   return {
     invokeSync(method, ...params) {
       accessHandle.write(new Uint8Array([0]), { at: 0 })
       accessHandle.truncate(1)
       accessHandle.flush()
-
       resultAccessHandle.truncate(0)
       resultAccessHandle.flush()
       Rpc.invoke(method, syncId, ...params)
-      console.time('wait')
       const maxDelay = 1_000
-      const hasResult = waitForSyncRpcResult(accessHandle, maxDelay, f)
+      const hasResult = waitForSyncRpcResult(accessHandle, maxDelay)
       if (!hasResult) {
         throw new Error(`Rpc error: timeout of ${maxDelay}ms exceeded`)
       }
-      console.timeEnd('wait')
-      console.log('done')
       const size = resultAccessHandle.getSize()
       const buf = new Uint8Array(size)
       resultAccessHandle.read(buf)
       const text = new TextDecoder().decode(buf)
       const parsed = JSON.parse(text)
-      draftHandle
-        .getFile()
-        .then((x) => x.size)
-        .then((x) => console.log({ x }))
       return parsed
     },
   }
--- packages/typescript-worker/src/parts/WaitForSyncRpcResult/WaitForSyncRpcResult.ts
+++ packages/typescript-worker/src/parts/WaitForSyncRpcResult/WaitForSyncRpcResult.ts
@@ -1,8 +1,4 @@
-export const waitForSyncRpcResult = (
-  handle: FileSystemSyncAccessHandle,
-  maxWaitTime: number,
-  file: File,
-): Promise<boolean> => {
+export const waitForSyncRpcResult = (handle: FileSystemSyncAccessHandle, maxWaitTime: number, file: File): boolean => {
   const start = Date.now()
   const end = start + maxWaitTime
   let errcount = 0
@@ -11,14 +7,10 @@ export const waitForSyncRpcResult = (
   while (true) {
     const now = Date.now()
     if (now >= end) {
-      console.log({ errcount, el: buffer[0], lastRead, size: file.size })
       return false
     }
-    // console.time('read')
     lastRead = handle.read(buffer, { at: 0 })
-    // console.timeEnd('read')
     if (buffer[0] === 1) {
-      console.log({ errcount, el: buffer[0], lastRead, size: file.size })
       return true
     }
     errcount++
