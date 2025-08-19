2c6d59cb733034ebedec53e8dbcb4cba61d446a1
Le Vivilet <levivlet@pm.me>
2025-08-19 11:40:26 +0200

    test

--- packages/e2e/src/typescript.completion-from-node-modules.ts
+++ packages/e2e/src/typescript.completion-from-node-modules.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.completion-from-node-modules'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect, Workspace }) => {
   // arrange
