0f7269d257219916c307c278bfa013d018df5640
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-19 15:45:01 +0200

    feature: use typescript worker for rename (#383)

--- packages/typescript-worker/src/parts/Rename/Rename.ts
+++ packages/typescript-worker/src/parts/Rename/Rename.ts
@@ -1,8 +1,5 @@
-import * as Assert from '../Assert/Assert.ts'
 import type { CommonRpc } from '../CommonRpc/CommonRpc.ts'
-import * as GetRenameFromTsResult from '../GetRenameFromTsResult/GetRenameFromTsResult.ts'
-import * as TextDocumentSync from '../TextDocumentSync/TextDocumentSync.ts'
-import type * as TypeScriptProtocol from '../TypeScriptProtocol/TypeScriptProtocol.ts'
+import { rename2 } from '../Rename2/Rename2.ts'
 
 export const rename = async (
   typeScriptRpc: CommonRpc,
@@ -11,15 +8,5 @@ export const rename = async (
   offset: number,
   newName: string,
 ) => {
-  const uri = textDocument.uri
-  Assert.string(uri)
-  await TextDocumentSync.openTextDocuments2(typeScriptRpc, [textDocument])
-  const tsPosition = await Position.getTsPosition(textDocument, offset)
-  const tsResult = await typeScriptRpc.invoke<TypeScriptProtocol.RenameInfo>('Rename.rename', {
-    file: textDocument.uri,
-    line: tsPosition.line,
-    offset: tsPosition.offset,
-  })
-  const rename = await GetRenameFromTsResult.getRenameResultFromTsResult(textDocument, Position, tsResult, newName)
-  return rename
+  return rename2(textDocument, offset, newName)
 }
