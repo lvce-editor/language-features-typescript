c5f784ed71b6e69021493a1ccd7dce3b9c497e3b
Le Vivilet <levivlet@pm.me>
2025-08-20 17:56:08 +0200

    debug

--- packages/typescript-worker/src/parts/Completion2/Completion2.ts
+++ packages/typescript-worker/src/parts/Completion2/Completion2.ts
@@ -7,7 +7,20 @@ export const getCompletion2 = async (textDocument: any, offset: number) => {
   Assert.string(uri)
   const { languageService, fs } = getOrCreateLanguageService(uri)
   fs.writeFile(textDocument.uri, textDocument.text)
-  const tsResult = languageService.getCompletionsAtPosition(textDocument.uri, offset, {})
+  const tsResult = languageService.getCompletionsAtPosition(textDocument.uri, offset, {
+    allowIncompleteCompletions: true,
+    allowRenameOfImportPath: true,
+    allowTextChangesInNewFiles: true,
+    autoImportFileExcludePatterns: [],
+    autoImportSpecifierExcludeRegexes: [],
+    importModuleSpecifierEnding: 'js',
+    includeCompletionsForImportStatements: true,
+    includePackageJsonAutoImports: 'on',
+    includeCompletionsWithInsertText: true,
+    includeCompletionsWithSnippetText: true,
+    importModuleSpecifierPreference: 'relative',
+    lazyConfiguredProjectsFromExternalProject: true,
+  })
   const completions = getCompletionFromTsResult2(tsResult)
   return completions
 }
