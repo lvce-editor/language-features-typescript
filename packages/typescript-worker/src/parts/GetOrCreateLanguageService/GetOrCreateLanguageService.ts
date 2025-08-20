3a3e5edc58ccd1f8a778d070c938f1b5f1e2d899
Le Vivilet <levivlet@pm.me>
2025-08-20 08:47:38 +0200

    feature: create language service per path

--- packages/typescript-worker/src/parts/GetOrCreateLanguageService/GetOrCreateLanguageService.ts
+++ packages/typescript-worker/src/parts/GetOrCreateLanguageService/GetOrCreateLanguageService.ts
@@ -1,22 +1,38 @@
 import type { LanguageService } from 'typescript'
 import { createTypeScriptLanguageService } from '../CreateTypeScriptLanguageService/CreateTypeScriptLanguageService.ts'
+import { getTsConfigPath } from '../GetTsconfigPath/GetTsconfigPath.ts'
 import * as LanguageServices from '../LanguageServices/LanguageServices.ts'
+import { parseTsconfig } from '../ParseTsconfig/ParseTsconfig.ts'
 
 // TODO cache it in a hashmap, one uri is connected to one project id
 // one project id is connected to one project
 
-let cache: LanguageService | undefined = undefined
+let nextProjectId = 1
+const projectCache: Record<number, LanguageService> = Object.create(null)
+const projectIdCache: Record<string, number> = Object.create(null)
 
 export const getOrCreateLanguageService = (uri: string) => {
   const id = 1
   const { fs, ts, client } = LanguageServices.get(id)
-  if (!cache) {
-    const languageService = createTypeScriptLanguageService(ts, fs, client)
-    cache = languageService
+  if (uri in projectCache) {
+    const projectId = projectIdCache[uri]
+    const languageService = projectCache[projectId]
+    return {
+      fs,
+      languageService,
+    }
   }
+  const exists = (uri: string) => client.invokeSync('SyncApi.exists', uri)
+  const readFile = (uri: string) => client.invokeSync('SyncApi.readFileSync', uri)
+  const tsConfigPath = getTsConfigPath(uri, exists)
+  const parsed = parseTsconfig(tsConfigPath, readFile)
+  const languageService = createTypeScriptLanguageService(ts, fs, client, parsed)
+  const projectId = nextProjectId++
+  projectCache[projectId] = languageService
+  projectIdCache[uri] = projectId
 
   return {
     fs,
-    languageService: cache,
+    languageService,
   }
 }
--- packages/typescript-worker/src/parts/TypeScriptLanguageHost/TypeScriptLanguageHost.ts
+++ packages/typescript-worker/src/parts/TypeScriptLanguageHost/TypeScriptLanguageHost.ts
@@ -1,4 +1,4 @@
-import type { LanguageServiceHost } from 'typescript'
+import type { LanguageServiceHost, ParsedCommandLine } from 'typescript'
 import type { IFileSystem } from '../IFileSystem/IFileSystem.ts'
 import { readLibFile } from '../ReadLibFile/ReadLibFile.ts'
 import type { SyncRpc } from '../SyncRpc/SyncRpc.ts'
@@ -9,14 +9,16 @@ export const create = (
   ts: typeof import('typescript'),
   fileSystem: IFileSystem,
   syncRpc: SyncRpc,
+  options: ParsedCommandLine,
 ): ILanguageServiceHost => {
   const languageServiceHost: ILanguageServiceHost = {
     getScriptKind(fileName) {
       return ts.ScriptKind.TS
     },
-    // getParsedCommandLine(fileName) {
-    //   return {}
-    // },
+
+    getParsedCommandLine(fileName) {
+      return options
+    },
     directoryExists(directoryName) {
       return true
     },
