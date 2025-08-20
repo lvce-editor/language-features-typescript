cdb2fb0cdfa5f9d588e3fde414d2289ad5c12d80
Le Vivilet <levivlet@pm.me>
2025-08-20 10:30:51 +0200

    undo

--- packages/typescript-worker/src/parts/CreateTypeScriptLanguageService/CreateTypeScriptLanguageService.ts
+++ packages/typescript-worker/src/parts/CreateTypeScriptLanguageService/CreateTypeScriptLanguageService.ts
@@ -1,4 +1,4 @@
-import type { LanguageService, LanguageServiceHost, ParsedCommandLine } from 'typescript'
+import type { LanguageService, LanguageServiceHost } from 'typescript'
 import type { IFileSystem } from '../IFileSystem/IFileSystem.ts'
 import type { SyncRpc } from '../SyncRpc/SyncRpc.ts'
 import * as TypeScriptLanguageHost from '../TypeScriptLanguageHost/TypeScriptLanguageHost.ts'
@@ -7,9 +7,8 @@ export const createTypeScriptLanguageService = (
   ts: typeof import('typescript'),
   fs: IFileSystem,
   client: SyncRpc,
-  config: ParsedCommandLine,
 ): LanguageService => {
-  const languageServiceHost: LanguageServiceHost = TypeScriptLanguageHost.create(ts, fs, client, config)
+  const languageServiceHost: LanguageServiceHost = TypeScriptLanguageHost.create(ts, fs, client)
   const languageService = ts.createLanguageService(languageServiceHost)
   return languageService
 }
