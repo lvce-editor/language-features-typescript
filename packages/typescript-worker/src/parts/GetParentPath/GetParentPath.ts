1be6288fdeb0189286c4d4dd8d336f843d26f95e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-20 10:42:11 +0200

    feature: improve config parsing (#396)

--- packages/typescript-worker/src/parts/GetParentPath/GetParentPath.ts
+++ packages/typescript-worker/src/parts/GetParentPath/GetParentPath.ts
@@ -3,5 +3,8 @@ export const getParentPath = (uri: string): string => {
   if (slashIndex === -1) {
     return ''
   }
+  if (uri[slashIndex - 1] === '/') {
+    return ''
+  }
   return uri.slice(0, slashIndex)
 }
--- packages/typescript-worker/src/parts/ParseTsconfig/ParseTsconfig.ts
+++ packages/typescript-worker/src/parts/ParseTsconfig/ParseTsconfig.ts
@@ -1,5 +1,4 @@
 import type { ParsedCommandLine } from 'typescript'
-import { getTsConfigPath } from '../GetTsconfigPath/GetTsconfigPath.ts'
 
 const emptyTsconfig: ParsedCommandLine = {
   options: {},
@@ -8,16 +7,20 @@ const emptyTsconfig: ParsedCommandLine = {
 }
 
 export const parseTsconfig = (tsconfigPath: string, readFile: (uri: string) => string): ParsedCommandLine => {
-  if (!getTsConfigPath) {
+  if (!tsconfigPath) {
     return emptyTsconfig
   }
-  const content = readFile(tsconfigPath)
-  const parsed = JSON.parse(content)
-  const options = parsed.compilerOptions || {}
-  const result: ParsedCommandLine = {
-    options,
-    errors: [],
-    fileNames: [],
+  try {
+    const content = readFile(tsconfigPath)
+    const parsed = JSON.parse(content)
+    const options = parsed.compilerOptions || {}
+    const result: ParsedCommandLine = {
+      options,
+      errors: [],
+      fileNames: [],
+    }
+    return result
+  } catch {
+    return emptyTsconfig
   }
-  return result
 }
