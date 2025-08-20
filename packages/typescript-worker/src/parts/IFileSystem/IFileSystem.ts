3f67cf2c486617db01142397b43040c9ec7b0024
Le Vivilet <levivlet@pm.me>
2025-08-20 15:14:11 +0200

    fix

--- packages/typescript-worker/src/parts/IFileSystem/IFileSystem.ts
+++ packages/typescript-worker/src/parts/IFileSystem/IFileSystem.ts
@@ -1,7 +1,6 @@
 export interface IFileSystem {
   readonly writeFile: (uri: string, content: string) => void
   readonly readFile: (uri: string) => string
-  readonly exists: (uri: string) => boolean
   readonly getVersion: () => string
   readonly getScriptFileNames: () => readonly string[]
   readonly getScriptVersion: (uri: string) => string
