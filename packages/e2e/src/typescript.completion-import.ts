984d7c9517cd855d42d9b8e866c950a50d60e29e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-21 09:07:32 +0200

    Discard changes to packages/e2e/src/typescript.completion-import.ts

--- packages/e2e/src/typescript.completion-import.ts
+++ packages/e2e/src/typescript.completion-import.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.completion-import'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
   // arrange
@@ -34,5 +34,5 @@ export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) 
   await expect(completions).toBeVisible()
   const completionItems = completions.locator('.EditorCompletionItem')
   await expect(completionItems).toHaveCount(1)
-  await expect(completionItems.nth(0)).toHaveText('add.js') // TODO should be ts
+  await expect(completionItems.nth(0)).toHaveText('add')
 }
