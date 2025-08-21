5d9af3a8bb207e66294e8bb6f7773cd1ed7a90c7
Le Vivilet <levivlet@pm.me>
2025-08-21 08:39:06 +0200

    feature: update completion options

--- packages/typescript-worker/src/parts/Completion2/Completion2.ts
+++ packages/typescript-worker/src/parts/Completion2/Completion2.ts
@@ -1,13 +1,16 @@
+import type { GetCompletionsAtPositionOptions } from 'typescript'
 import * as Assert from '../Assert/Assert.ts'
 import { getCompletionFromTsResult2 } from '../GetCompletionFromTsResult2/GetCompletionFromTsResult2.ts'
 import { getOrCreateLanguageService } from '../GetOrCreateLanguageService/GetOrCreateLanguageService.ts'
+import { getCompletionOptions } from '../GetCompletionOptions/GetCompletionOptions.ts'
 
 export const getCompletion2 = async (textDocument: any, offset: number) => {
   const uri = textDocument.uri
   Assert.string(uri)
   const { languageService, fs } = getOrCreateLanguageService(uri)
   fs.writeFile(textDocument.uri, textDocument.text)
-  const tsResult = languageService.getCompletionsAtPosition(textDocument.uri, offset, {})
+  const completionOptions = getCompletionOptions()
+  const tsResult = languageService.getCompletionsAtPosition(textDocument.uri, offset, completionOptions)
   const completions = getCompletionFromTsResult2(tsResult)
   return completions
 }
