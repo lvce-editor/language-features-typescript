4a686a0900bba4533a98b4d6bd95ba9e612e8494
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-21 09:07:56 +0200

    Discard changes to packages/typescript-worker/src/parts/GetLibFileUrl/GetLibFileUrl.ts

--- packages/typescript-worker/src/parts/GetLibFileUrl/GetLibFileUrl.ts
+++ packages/typescript-worker/src/parts/GetLibFileUrl/GetLibFileUrl.ts
@@ -1,21 +1,16 @@
-const libString = 'node_modules/@typescript/lib'
-const libStringLength = libString.length
-
-export const getLibFileUrl = (uri: string): string => {
+export const getLibFileUrl = (uri) => {
   if (uri === 'lib.d.ts' || uri === '/lib.d.ts') {
     return new URL('../../../node_modules/typescript/lib/lib.d.ts', import.meta.url).toString()
   }
-  const index = uri.indexOf(libString)
-  if (index === -1) {
-    return ''
+  if (uri.startsWith('node_modules/@typescript/lib')) {
+    const relativePath =
+      uri.slice('node_modules/@typescript/lib'.length, -3).replaceAll('-', '.').replaceAll('/', '.') + '.d.ts'
+    return new URL(`../../../node_modules/typescript/lib/lib${relativePath}`, import.meta.url).toString()
   }
-  const relativePath =
-    'lib' +
-    uri
-      .slice(index + libStringLength)
-      .replaceAll('-', '.')
-      .replaceAll('/', '.')
-      .replaceAll('.ts', '') +
-    '.d.ts'
-  return new URL(`../../../node_modules/typescript/lib/${relativePath}`, import.meta.url).toString()
+  if (uri.startsWith('/node_modules/@typescript/lib')) {
+    const relativePath =
+      uri.slice('/node_modules/@typescript/lib'.length, -3).replaceAll('-', '.').replaceAll('/', '.') + '.d.ts'
+    return new URL(`../../../node_modules/typescript/lib/lib${relativePath}`, import.meta.url).toString()
+  }
+  return ''
 }
