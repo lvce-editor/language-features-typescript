06f7c275b8c5358494327d76cbf7e48bf2e2a0b5
Le Vivilet <levivlet@pm.me>
2025-08-21 08:38:26 +0200

    move code

--- packages/typescript-worker/src/parts/Completion2/Completion2.ts
+++ packages/typescript-worker/src/parts/Completion2/Completion2.ts
@@ -2,26 +2,7 @@ import type { GetCompletionsAtPositionOptions } from 'typescript'
 import * as Assert from '../Assert/Assert.ts'
 import { getCompletionFromTsResult2 } from '../GetCompletionFromTsResult2/GetCompletionFromTsResult2.ts'
 import { getOrCreateLanguageService } from '../GetOrCreateLanguageService/GetOrCreateLanguageService.ts'
-
-const getCompletionOptions = (): GetCompletionsAtPositionOptions => {
-  return {
-    allowIncompleteCompletions: true,
-    allowRenameOfImportPath: true,
-    allowTextChangesInNewFiles: true,
-    autoImportFileExcludePatterns: [],
-    autoImportSpecifierExcludeRegexes: [],
-    importModuleSpecifierEnding: 'js',
-    includeCompletionsForImportStatements: true,
-    includePackageJsonAutoImports: 'on',
-    includeCompletionsWithInsertText: true,
-    includeCompletionsWithSnippetText: true,
-    includeCompletionsForModuleExports: true,
-    includeAutomaticOptionalChainCompletions: true,
-    includeCompletionsWithObjectLiteralMethodSnippets: true,
-    // importModuleSpecifierPreference: 'relative',
-    // lazyConfiguredProjectsFromExternalProject: true,
-  }
-}
+import { getCompletionOptions } from '../GetCompletionOptions/GetCompletionOptions.ts'
 
 export const getCompletion2 = async (textDocument: any, offset: number) => {
   const uri = textDocument.uri
