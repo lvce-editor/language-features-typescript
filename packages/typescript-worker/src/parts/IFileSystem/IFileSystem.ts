0ee2eef621422cb409bced9d7811dbe21ec2441b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-21 12:16:32 +0200

    Delete packages/typescript-worker/src/parts/IFileSystem/IFileSystem.ts

--- packages/typescript-worker/src/parts/IFileSystem/IFileSystem.ts
+++
@@ -1,7 +0,0 @@
-export interface IFileSystem {
-  readonly writeFile: (uri: string, content: string) => void
-  readonly readFile: (uri: string) => string
-  readonly getVersion: () => string
-  readonly getScriptFileNames: () => readonly string[]
-  readonly getScriptVersion: (uri: string) => string
-}
