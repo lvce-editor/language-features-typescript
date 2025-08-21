eccd652c6ce171b34dabfa886647bfb395d23476
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-21 08:20:00 +0200

    feature: try to implement selection api (#368)
    * feature: try to implement selection api
    
    * improve test
    
    * Discard changes to packages/typescript-worker/src/parts/Completion2/Completion2.ts
    
    * test
    
    * fix test
    
    * tsc
    
    * test
    
    * test
    
    * simplify code

--- packages/typescript-worker/src/parts/Selection/Selection.ts
+++ packages/typescript-worker/src/parts/Selection/Selection.ts
@@ -1,27 +1,5 @@
 import type { CommonRpc } from '../CommonRpc/CommonRpc.ts'
-import * as GetPositionsFromTsResult from '../GetPositionsFromTsResult/GetPositionsFromTsResult.ts'
-import * as TextDocumentSync from '../TextDocumentSync/TextDocumentSync.ts'
-import type * as TypeScriptProtocol from '../TypeScriptProtocol/TypeScriptProtocol.ts'
-
-const getLocations = (positions: any) => {
-  const locations = []
-  let last = {
-    line: 0,
-    offset: 0,
-  }
-  for (let i = 0; i < positions.length; i += 2) {
-    const next = {
-      line: positions[i] + 1,
-      offset: positions[i + 1] + 1,
-    }
-    if (next.line === last.line && next.offset === last.offset) {
-      continue
-    }
-    last = next
-    locations.push(next)
-  }
-  return locations
-}
+import { expandSelection2 } from '../Selection2/Selection2.ts'
 
 export const expandSelection = async (
   typeScriptRpc: CommonRpc,
@@ -29,15 +7,5 @@ export const expandSelection = async (
   textDocument: any,
   positions: Uint32Array,
 ) => {
-  await TextDocumentSync.openTextDocuments2(typeScriptRpc, [textDocument])
-  const locations = getLocations(positions)
-  const tsResult = await typeScriptRpc.invoke<readonly TypeScriptProtocol.SelectionRange[]>(
-    'Selection.expandSelection',
-    {
-      file: textDocument.uri,
-      locations,
-    },
-  )
-  const newPositions = GetPositionsFromTsResult.getPositionsFromTsResult(positions, tsResult)
-  return newPositions
+  return expandSelection2(textDocument, positions)
 }
