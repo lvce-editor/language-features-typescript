77147169eba8105a6489d68bd13f0e6837301b6a
Le Vivilet <levivlet@pm.me>
2025-08-21 12:20:34 +0200

    test

--- packages/e2e/src/typescript.completion-import.ts
+++ packages/e2e/src/typescript.completion-import.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.completion-import'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
   // arrange
