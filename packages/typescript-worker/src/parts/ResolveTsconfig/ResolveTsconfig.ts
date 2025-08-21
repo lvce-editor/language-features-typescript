11fa2f631a504bc7591502cb5bb22e9b66f96577
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-21 12:33:45 +0200

    feature: improve tsconfig resolution (#418)
    * feature: create language service per path
    
    * fix: tsconfig
    
    * handle error
    
    * fix config
    
    * remove unused code
    
    * fix path check
    
    * feature: improve typescript module resolution
    
    * debug
    
    * debug
    
    * remove log
    
    * debug
    
    * debug
    
    * add tsconfig for test
    
    * fix resolution
    
    * Discard changes to packages/typescript-worker/src/parts/CreateFileSystem/CreateFileSystem.ts
    
    * move function
    
    * simplify config
    
    * test
    
    * fix
    
    * fix
    
    * remove log
    
    * remove log
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * remove unused
    
    * fix: lib not working when config has configPath property
    
    * fix
    
    * fix: file not found
    
    * fix typo
    
    * add test
    
    * improve test
    
    * fix auto import
    
    * fix
    
    * move code
    
    * Discard changes to packages/extension/src/parts/SyncApi/SyncApi.js
    
    * tsc
    
    * log
    
    * tsc
    
    * test
    
    * test
    
    * handle error
    
    * simplify code
    
    * fix
    
    * use error handle
    
    * debug
    
    * fix
    
    * update max delay
    
    * feature: write result to position zero
    
    * test
    
    * feature: update server
    
    * Discard changes to packages/e2e/src/typescript.completion-import.ts
    
    * Discard changes to packages/typescript-worker/src/parts/TypeScriptLanguageHost/TypeScriptLanguageHost.ts
    
    * tsc
    
    * tsc
    
    * fix

--- packages/typescript-worker/src/parts/ResolveTsconfig/ResolveTsconfig.ts
+++ packages/typescript-worker/src/parts/ResolveTsconfig/ResolveTsconfig.ts
@@ -1,29 +1,39 @@
-import type { ParsedCommandLine } from 'typescript'
+import type { CompilerOptions, ParseConfigHost, ParsedCommandLine } from 'typescript'
+import { emptyTsconfig } from '../EmptyTsConfig/EmptyTsConfig.ts'
 import { getFiles } from '../GetFiles/GetFiles.ts'
 import { getParentPath } from '../GetParentPath/GetParentPath.ts'
-import { emptyTsconfig } from '../EmptyTsConfig/EmptyTsConfig.ts'
 
 export const resolveTsconfig = (
   tsconfigPath: string,
   parsed: any,
   readFile: (uri: string) => string,
   readDir: (uri: string) => readonly string[],
+  fileExists: (uri: string) => boolean,
+  ts: typeof import('typescript'),
 ): ParsedCommandLine => {
   if (!tsconfigPath) {
     return emptyTsconfig
   }
   try {
     const rootDir = getParentPath(tsconfigPath)
-    let options = parsed.compilerOptions || {}
+    // ts.parseJsonConfigFileContent(fileName, jsonText).fileNames
+    const host: ParseConfigHost = {
+      fileExists,
+      readDirectory: readDir,
+      readFile,
+      useCaseSensitiveFileNames: false,
+    }
+    const existingOptions = {}
+    const config2 = ts.parseJsonConfigFileContent(parsed, host, rootDir, existingOptions, tsconfigPath)
+    let options: CompilerOptions = config2.options
     options = {
       ...options,
       rootDir,
-      configFilePath: tsconfigPath,
     }
 
     const dirname = getParentPath(tsconfigPath)
     const include = options.include || []
-    const files = getFiles(dirname, include, readDir)
+    const files = getFiles(dirname, include as string[], readDir)
     const result: ParsedCommandLine = {
       options,
       errors: [],
