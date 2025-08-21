55de54110afec68032c4ac498635984779c713df
Le Vivilet <levivlet@pm.me>
2025-08-21 08:09:23 +0200

    test

--- packages/e2e/src/typescript.add-missing-imports.ts
+++ packages/e2e/src/typescript.add-missing-imports.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.add-missing-imports'
 
-// export const skip = true
+export const skip = true
 
 export const test: Test = async ({ FileSystem, Main, Editor }) => {
   // arrange
