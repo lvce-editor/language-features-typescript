1c0c91670221164a49547ced092758eb217d3c01
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-21 09:08:13 +0200

    Discard changes to packages/typescript-worker/src/parts/ResolveTsconfig/ResolveTsconfig.ts

--- packages/typescript-worker/src/parts/ResolveTsconfig/ResolveTsconfig.ts
+++ packages/typescript-worker/src/parts/ResolveTsconfig/ResolveTsconfig.ts
@@ -1,39 +1,29 @@
-import type { CompilerOptions, ParseConfigHost, ParsedCommandLine } from 'typescript'
-import { emptyTsconfig } from '../EmptyTsConfig/EmptyTsConfig.ts'
+import type { ParsedCommandLine } from 'typescript'
 import { getFiles } from '../GetFiles/GetFiles.ts'
 import { getParentPath } from '../GetParentPath/GetParentPath.ts'
+import { emptyTsconfig } from '../EmptyTsConfig/EmptyTsConfig.ts'
 
 export const resolveTsconfig = (
   tsconfigPath: string,
   parsed: any,
   readFile: (uri: string) => string,
   readDir: (uri: string) => readonly string[],
-  fileExists: (uri: string) => boolean,
-  ts: typeof import('typescript'),
 ): ParsedCommandLine => {
   if (!tsconfigPath) {
     return emptyTsconfig
   }
   try {
     const rootDir = getParentPath(tsconfigPath)
-    // ts.parseJsonConfigFileContent(fileName, jsonText).fileNames
-    const host: ParseConfigHost = {
-      fileExists,
-      readDirectory: readDir,
-      readFile,
-      useCaseSensitiveFileNames: false,
-    }
-    const existingOptions = {}
-    const config2 = ts.parseJsonConfigFileContent(parsed, host, rootDir, existingOptions, tsconfigPath)
-    let options: CompilerOptions = config2.options
+    let options = parsed.compilerOptions || {}
     options = {
       ...options,
       rootDir,
+      configFilePath: tsconfigPath,
     }
 
     const dirname = getParentPath(tsconfigPath)
     const include = options.include || []
-    const files = getFiles(dirname, include as string[], readDir)
+    const files = getFiles(dirname, include, readDir)
     const result: ParsedCommandLine = {
       options,
       errors: [],
