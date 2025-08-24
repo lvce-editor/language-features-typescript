757ed7ea760425fdef6be26f100077da2a47fe84
Le Vivilet <levivlet@pm.me>
2025-08-24 20:06:24 +0200

    tsc

--- packages/extension/src/parts/GetRpc/GetRpc.js
+++ packages/extension/src/parts/GetRpc/GetRpc.js
@@ -1,8 +1,6 @@
-import * as GetRpcNode from '../GetRpcNode/GetRpcNode.js'
 import * as GetRpcWeb from '../GetRpcWeb/GetRpcWeb.js'
-import * as IsWeb from '../IsWeb/IsWeb.js'
 
 export const getRpc = (path) => {
-  const module = IsWeb.isWeb ? GetRpcWeb : GetRpcNode
+  const module = GetRpcWeb
   return module.getRpc(path)
 }
