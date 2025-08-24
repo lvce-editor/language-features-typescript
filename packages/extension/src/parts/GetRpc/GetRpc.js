663fa5903049e32cfe4e3ffc5e908fae50a4f503
Le Vivilet <levivlet@pm.me>
2025-08-24 20:06:35 +0200

    tsc

--- packages/extension/src/parts/GetRpc/GetRpc.js
+++ packages/extension/src/parts/GetRpc/GetRpc.js
@@ -1,6 +1,5 @@
 import * as GetRpcWeb from '../GetRpcWeb/GetRpcWeb.js'
 
 export const getRpc = (path) => {
-  const module = GetRpcWeb
-  return module.getRpc(path)
+  return GetRpcWeb.getRpc(path)
 }
