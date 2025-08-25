78f07c841508dcdf485205b7c8af69120cc443b8
Le Vivilet <levivlet@pm.me>
2025-08-25 08:40:13 +0200

    feature: add node modules completion test

--- packages/e2e/src/typescript.completion-from-node-modules.ts
+++ packages/e2e/src/typescript.completion-from-node-modules.ts
@@ -2,11 +2,22 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.completion-from-node-modules'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/tsconfig.json`,
+    JSON.stringify(
+      {
+        compilerOptions: {
+          lib: ['esnext'],
+          types: [],
+        },
+      },
+      null,
+      2,
+    ),
+  )
   await FileSystem.writeFile(`${tmpDir}/package.json`, `{ "type": "module", "dependencies": { "lodash": "^1.0.0" } }`)
   await FileSystem.writeFile(`${tmpDir}/node_modules/lodash/package.json`, `{ "main": "index.js", "type": "module" }`)
   await FileSystem.writeFile(`${tmpDir}/node_modules/lodash/index.js`, `export const add = (a,b) => a + b`)
