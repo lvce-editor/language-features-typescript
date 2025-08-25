88d713f55ff9e9b1ed9936df5e4b4ee52ce68048
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-25 08:31:28 +0200

    feature: simplify build (#429)
    * feature: simplify build
    
    * fix

--- packages/extension/src/parts/GetRpc/GetRpc.js
+++
@@ -1,5 +0,0 @@
-import * as GetRpcWeb from '../GetRpcWeb/GetRpcWeb.js'
-
-export const getRpc = (path) => {
-  return GetRpcWeb.getRpc(path)
-}
--- packages/extension/src/parts/Rpc/Rpc.js
+++ packages/extension/src/parts/Rpc/Rpc.js
@@ -1,15 +1,6 @@
-import * as GetRpc from '../GetRpc/GetRpc.js'
-
-export const state = {
-  rpc: undefined,
-}
-
-export const listen = async ({ path }) => {
-  state.rpc = await GetRpc.getRpc(path)
-}
+import * as TypeScriptWorker from '../TypeScriptWorker/TypeScriptWorker.js'
 
 export const invoke = async (method, ...params) => {
-  const rpc = state.rpc
-  const result = await state.rpc.invoke(method, ...params)
-  return result
+  const rpc = await TypeScriptWorker.getInstance()
+  return rpc.invoke(method, ...params)
 }
