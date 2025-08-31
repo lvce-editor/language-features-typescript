c0d6f73041cdd5cde72211e05271c673fbb9279d
Le Vivilet <levivlet@pm.me>
2025-08-31 20:59:52 +0200

    feature: improve references test

--- packages/e2e/src/typescript.references.ts
+++ packages/e2e/src/typescript.references.ts
@@ -1,7 +1,5 @@
 export const name = 'typescript.references'
 
-// export const skip = 1
-
 export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
@@ -40,18 +38,11 @@ add(1,2)
   // assert
   const viewletLocations = Locator('.Locations')
   await expect(viewletLocations).toBeVisible()
-  // const viewletReferencesMessage = Locator('.LocationsMessage')
-  // await expect(viewletReferencesMessage).toHaveText('3 results in 2 files') TODO
+  const viewletReferencesMessage = Locator('.LocationsMessage')
+  await expect(viewletReferencesMessage).toHaveText('3 results in 2 files')
   const referenceItems = viewletLocations.locator('.TreeItem')
-  // await expect(referenceItems).toHaveCount(5) TODO
   const referenceItemOne = referenceItems.nth(0)
   await expect(referenceItemOne).toHaveText('test.ts')
   const referenceItemTwo = referenceItems.nth(1)
   await expect(referenceItemTwo).toHaveText(`import { add } from './add.ts'`)
-  // const referenceItemThree = referenceItems.nth(2)
-  // await expect(referenceItemThree).toHaveText(`add(1,2)`)// TODO
-  // const referenceItemFour = referenceItems.nth(3)
-  // await expect(referenceItemFour).toHaveText(`add.ts`)
-  // const referenceItemFive = referenceItems.nth(4)
-  // await expect(referenceItemFive).toHaveText(`export const add = () => {}`)
 }
