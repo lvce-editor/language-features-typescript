eceb2019f1a16d401dddd825425b8c9284c03342
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-31 21:07:28 +0200

    feature: add references error e2e test (#447)

---
+++ packages/e2e/src/typescript.references-error.ts
@@ -0,0 +1,42 @@
+export const name = 'typescript.references'
+
+export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/tsconfig.json`,
+    JSON.stringify(
+      {
+        compilerOptions: {
+          lib: ['esnext'],
+          types: [],
+        },
+      },
+      null,
+      2,
+    ),
+  )
+  await FileSystem.writeFile(
+    `${tmpDir}/add.ts`,
+    `export const add = () => {}
+`,
+  )
+  await FileSystem.writeFile(
+    `${tmpDir}/test.ts`,
+    `import { add } from './not-found.ts'
+
+add(1,2)
+`,
+  )
+  await FileSystem.writeFile(`${tmpDir}/tsconfig.json`, `{}`)
+  await Main.openUri(`${tmpDir}/test.ts`)
+  await Editor.setCursor(2, 2)
+
+  // act
+  await Editor.findAllReferences()
+
+  // assert
+  const viewletError = Locator('.Viewlet.Error')
+  await expect(viewletError).toBeVisible()
+  await expect(viewletError).toHaveText('Error: Unexpected end of JSON input') // TODO
+}
