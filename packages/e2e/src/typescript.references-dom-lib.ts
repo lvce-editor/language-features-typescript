1cb9d3aa6bc22fd82786842002dfdcfa66e4edb6
Le Vivilet <levivilet@pm.me>
2025-09-07 12:14:03 +0200

    feature: add dom lib e2e test

---
+++ packages/e2e/src/typescript.references-dom-lib.ts
@@ -0,0 +1,44 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'typescript.references-dom-lib'
+
+export const skip = 1
+
+export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/tsconfig.json`,
+    JSON.stringify(
+      {
+        compilerOptions: {
+          lib: ['esnext', 'dom'],
+          types: [],
+        },
+      },
+      null,
+      2,
+    ),
+  )
+  await FileSystem.writeFile(
+    `${tmpDir}/test.ts`,
+    `window
+`,
+  )
+  await Main.openUri(`${tmpDir}/test.ts`)
+  await Editor.setCursor(0, 2)
+
+  // act
+  await Editor.findAllReferences()
+
+  // assert
+  const viewletLocations = Locator('.Locations')
+  await expect(viewletLocations).toBeVisible()
+  const viewletReferencesMessage = Locator('.LocationsMessage')
+  await expect(viewletReferencesMessage).toHaveText('3 results in 2 files')
+  const referenceItems = viewletLocations.locator('.TreeItem')
+  const referenceItemOne = referenceItems.nth(0)
+  await expect(referenceItemOne).toHaveText('test.ts')
+  const referenceItemTwo = referenceItems.nth(1)
+  await expect(referenceItemTwo).toHaveText(`import { add } from './add.ts'`)
+}
