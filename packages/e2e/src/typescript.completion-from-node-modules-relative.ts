15b8d768da835b649727a5a76b6f73c4a465e38d
Le Vivilet <levivilet@pm.me>
2025-09-20 14:54:37 +0200

    feature: add node modules relative completion test (#470)

---
+++ packages/e2e/src/typescript.completion-from-node-modules-relative.ts
@@ -0,0 +1,26 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'typescript.completion-from-node-modules-relative'
+
+export const skip = 1
+
+export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/package.json`, `{ "type": "module", "dependencies": { "lodash": "^1.0.0" } }`)
+  await FileSystem.writeFile(`${tmpDir}/node_modules/lodash/package.json`, `{ "main": "index.js", "type": "module" }`)
+  await FileSystem.writeFile(`${tmpDir}/node_modules/lodash/index.js`, `export const add = (a,b) => a + b`)
+  await FileSystem.writeFile(`${tmpDir}/test.ts`, `import * as _ from ''`)
+  await Main.openUri(`${tmpDir}/test.ts`)
+  await Editor.setCursor(0, 20)
+
+  // act
+  await Editor.openCompletion()
+
+  // assert
+  const completions = Locator('#Completions')
+  await expect(completions).toBeVisible()
+  const completionItems = completions.locator('.EditorCompletionItem')
+  await expect(completionItems).toHaveCount(1)
+  await expect(completionItems.nth(0)).toHaveText('lodash')
+}
