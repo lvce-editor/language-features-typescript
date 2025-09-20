d2764728431bf2d80d140cccc8e10727a979bbcf
Le Vivilet <levivilet@pm.me>
2025-09-20 18:26:29 +0200

    test

--- packages/e2e/src/typescript.definition-imported-file.ts
+++ packages/e2e/src/typescript.definition-imported-file.ts
@@ -2,14 +2,13 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.definition-imported-file'
 
-export const skip = 1
 export const test: Test = async ({ Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/definition-imported-file').toString()
   const workspaceUrl = Workspace.resolveFileUrl(fixtureUrl)
   await Workspace.setPath(workspaceUrl)
   await Main.openUri(`${workspaceUrl}/src/test.ts`)
-  await Editor.setCursor(0, 3)
+  await Editor.setCursor(2, 3)
 
   // act
   await Editor.goToDefinition()
@@ -18,5 +17,5 @@ export const test: Test = async ({ Workspace, Main, Editor, Locator, expect }) =
   const mainTabs = Locator('.MainTab')
   await expect(mainTabs).toHaveCount(2)
   const mainTabTwo = mainTabs.nth(1)
-  await expect(mainTabTwo).toHaveText('lib.dom.d.ts')
+  await expect(mainTabTwo).toHaveText('add.ts')
 }
