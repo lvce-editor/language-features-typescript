746617b65f0969dd950b469ad03a82a95d722bb4
Le Vivilet <levivilet@pm.me>
2025-09-20 11:11:09 +0200

    test

--- packages/e2e/src/typescript.auto-import.ts
+++ packages/e2e/src/typescript.auto-import.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.auto-import'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
