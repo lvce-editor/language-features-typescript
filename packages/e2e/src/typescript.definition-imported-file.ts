779fd99d3640d9a44c881432284db852124d0d01
Le Vivilet <levivilet@pm.me>
2025-09-20 18:29:12 +0200

    feature: add definition import test (#482)
    * feature: add definition import test
    
    * test

---
+++ packages/e2e/src/typescript.definition-imported-file.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'typescript.definition-imported-file'
+
+export const test: Test = async ({ Workspace, Main, Editor, Locator, expect }) => {
+  // arrange
+  const fixtureUrl = import.meta.resolve('../fixtures/definition-imported-file').toString()
+  const workspaceUrl = Workspace.resolveFileUrl(fixtureUrl)
+  await Workspace.setPath(workspaceUrl)
+  await Main.openUri(`${workspaceUrl}/src/test.ts`)
+  await Editor.setCursor(2, 3)
+
+  // act
+  await Editor.goToDefinition()
+
+  // assert
+  const mainTabs = Locator('.MainTab')
+  await expect(mainTabs).toHaveCount(2)
+  const mainTabTwo = mainTabs.nth(1)
+  await expect(mainTabTwo).toHaveText('add.ts')
+}
