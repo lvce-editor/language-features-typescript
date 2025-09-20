660f68122922dba96adddc18c7fc6f7ab00c6d21
Le Vivilet <levivilet@pm.me>
2025-09-20 15:41:08 +0200

    feature: resolve files starting with lib (#480)

--- packages/typescript-worker/src/parts/GetLibFileUrl/GetLibFileUrl.ts
+++ packages/typescript-worker/src/parts/GetLibFileUrl/GetLibFileUrl.ts
@@ -5,6 +5,9 @@ export const getLibFileUrl = (uri: string): string => {
   if (uri === 'lib.d.ts' || uri === '/lib.d.ts') {
     return new URL('../../../node_modules/typescript/lib/lib.d.ts', import.meta.url).toString()
   }
+  if (uri.startsWith('lib.')) {
+    return new URL(`../../../node_modules/typescript/lib/${uri}`, import.meta.url).toString()
+  }
   const index = uri.indexOf(libString)
   if (index === -1) {
     return ''
@@ -17,5 +20,6 @@ export const getLibFileUrl = (uri: string): string => {
       .replaceAll('/', '.')
       .replaceAll('.ts', '') +
     '.d.ts'
+
   return new URL(`../../../node_modules/typescript/lib/${relativePath}`, import.meta.url).toString()
 }
