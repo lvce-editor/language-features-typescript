a859d652da429e8321ebd2ee318fee909b64612f
Le Vivilet <levivilet@pm.me>
2025-09-20 15:20:44 +0200

    skp

--- packages/e2e/src/typescript.no-reference-found.ts
+++ packages/e2e/src/typescript.no-reference-found.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.no-reference-found'
 
+export const skip = 1
+
 export const test: Test = async ({ Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/no-reference-found').toString()
