6d9771fd4c3f4a8c1243b83282b429643eeeadfb
Le Vivilet <levivilet@pm.me>
2025-09-23 16:24:21 +0200

    feature: use new fixtures api

--- packages/e2e/src/typescript.references-empty.ts
+++ packages/e2e/src/typescript.references-empty.ts
@@ -2,10 +2,10 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.references-empty'
 
-export const test: Test = async ({ Workspace, Main, Editor, Locator, expect }) => {
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/references-empty').toString()
-  const workspaceUrl = Workspace.resolveFileUrl(fixtureUrl)
+  const workspaceUrl = await FileSystem.loadFixture(fixtureUrl)
   await Workspace.setPath(workspaceUrl)
   await Main.openUri(`${workspaceUrl}/src/test.ts`)
   await Editor.setCursor(1, 0)
