6977455aa436a5933562e5826e66af290dea0ac2
Le Vivilet <levivilet@pm.me>
2025-09-23 16:23:48 +0200

    feature: use new fixtures api

--- packages/e2e/src/typescript.organize-imports.ts
+++ packages/e2e/src/typescript.organize-imports.ts
@@ -4,10 +4,10 @@ export const name = 'typescript.organize-imports'
 
 // export const skip = true
 
-export const test: Test = async ({ Main, Editor, Workspace }) => {
+export const test: Test = async ({ FileSystem, Main, Editor, Workspace }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/organize-imports').toString()
-  const workspaceUrl = Workspace.resolveFileUrl(fixtureUrl)
+  const workspaceUrl = await FileSystem.loadFixture(fixtureUrl)
   await Workspace.setPath(workspaceUrl)
   await Main.openUri(`${workspaceUrl}/src/c.ts`)
 
