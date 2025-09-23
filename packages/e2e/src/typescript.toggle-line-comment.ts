b75de74d89907bd2a143f00f2fb8d132663d571a
Le Vivilet <levivilet@pm.me>
2025-09-23 16:26:45 +0200

    feature: use new fixtures api

--- packages/e2e/src/typescript.toggle-line-comment.ts
+++ packages/e2e/src/typescript.toggle-line-comment.ts
@@ -2,10 +2,10 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.toggle-line-comment'
 
-export const test: Test = async ({ Workspace, Main, Editor }) => {
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/toggle-line-comment').toString()
-  const workspaceUrl = Workspace.resolveFileUrl(fixtureUrl)
+  const workspaceUrl = await FileSystem.loadFixture(fixtureUrl)
   await Workspace.setPath(workspaceUrl)
   await Main.openUri(`${workspaceUrl}/src/test.ts`)
   await Editor.setCursor(0, 3)
