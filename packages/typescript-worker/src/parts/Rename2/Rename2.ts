15a45682ac22603c5e42cc3d206795ac159eacec
Le Vivilet <levivilet@pm.me>
2025-09-24 23:44:58 +0200

    tsc

--- packages/typescript-worker/src/parts/Rename2/Rename2.ts
+++ packages/typescript-worker/src/parts/Rename2/Rename2.ts
@@ -9,6 +9,6 @@ export const rename2 = async (textDocument: any, offset: number, newName: string
   fs.writeFile(textDocument.uri, textDocument.text)
   const tsResult = languageService.getRenameInfo(textDocument.uri, offset, {})
   const tsLocations = languageService.findRenameLocations(textDocument.uri, offset, false, false, {})
-  const rename = await getRenameResultFromTsResult2(textDocument.text, tsResult, tsLocations, newName)
+  const rename = await getRenameResultFromTsResult2(textDocument.text, tsResult, tsLocations as any, newName)
   return rename
 }
