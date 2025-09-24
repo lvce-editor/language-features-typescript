e3857ec7899eaea6d4fe3570314162d41de8ba19
Le Vivilet <levivilet@pm.me>
2025-09-24 23:44:44 +0200

    tsc

--- packages/typescript-worker/src/parts/GetDiagnosticFromTsResult2/GetDiagnosticFromTsResult2.ts
+++ packages/typescript-worker/src/parts/GetDiagnosticFromTsResult2/GetDiagnosticFromTsResult2.ts
@@ -26,7 +26,7 @@ const convertTsDiagnostic = (text: string, diagnostic: ts.DiagnosticWithLocation
 export const getDiagnosticsFromTsResult2 = (text: string, tsResult: readonly ts.Diagnostic[]) => {
   const diagnostics: any[] = []
   for (const tsDiagnostic of tsResult) {
-    diagnostics.push(convertTsDiagnostic(text, tsDiagnostic))
+    diagnostics.push(convertTsDiagnostic(text, tsDiagnostic as any))
   }
   return diagnostics
 }
