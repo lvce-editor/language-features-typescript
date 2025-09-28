1f41489a12bf90c92d36adb4730c97e60fefb9cf
Le Vivilet <levivilet@pm.me>
2025-09-28 12:58:24 +0200

    feature: enable no reference found e2e test (#553)

--- packages/e2e/src/typescript.no-reference-found.ts
+++ packages/e2e/src/typescript.no-reference-found.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.no-reference-found'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/no-reference-found')
