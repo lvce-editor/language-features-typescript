c6cb8d20030a9272a937fc49f618f5c84216ace5
Le Vivilet <levivilet@pm.me>
2025-09-28 13:05:39 +0200

    feature: add no definition found test

--- packages/e2e/src/typescript.no-definition-found.ts
+++ packages/e2e/src/typescript.no-definition-found.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.no-definition-found'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/no-definition-found')
