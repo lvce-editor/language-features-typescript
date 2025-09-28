54f0adac2e6f0d30cdbcb1d4322bfaed420ae39d
Le Vivilet <levivilet@pm.me>
2025-09-28 19:17:41 +0200

    feature: update comment api

--- packages/extension/src/parts/ExtensionHost/ExtensionHostCommentProviderTypeScript.ts
+++ packages/extension/src/parts/ExtensionHost/ExtensionHostCommentProviderTypeScript.ts
@@ -3,7 +3,7 @@ import * as TypeScriptWorker from '../TypeScriptWorker/TypeScriptWorker.ts'
 
 export const languageId = LanguageId.TypeScript
 
-export const provideComments = async (textDocument: any, offset: number): Promise<any> => {
+export const provideComment = async (textDocument: any, offset: number): Promise<any> => {
   const worker = await TypeScriptWorker.getInstance()
   return worker.invoke('Comment.provide', textDocument, offset)
 }
