26d2fc543aa38e901a2c0177d5693c9e0e3e6e42
Le Vivilet <levivilet@pm.me>
2025-09-28 19:06:35 +0200

    feature: ask worker for comments

--- packages/extension/src/parts/ExtensionHost/ExtensionHostCommentProviderTypeScript.ts
+++ packages/extension/src/parts/ExtensionHost/ExtensionHostCommentProviderTypeScript.ts
@@ -1,8 +1,9 @@
 import * as LanguageId from '../LanguageId/LanguageId.ts'
+import * as TypeScriptWorker from '../TypeScriptWorker/TypeScriptWorker.ts'
 
 export const languageId = LanguageId.TypeScript
 
 export const provideComments = async (textDocument: any, offset: number): Promise<any> => {
-  console.log({ textDocument, offset })
-  return []
+  const worker = await TypeScriptWorker.getInstance()
+  return worker.invoke('Comment.provide', textDocument, offset)
 }
