3f4b6b118f3650637fc402c2b4f8df8912b246e3
Le Vivilet <levivilet@pm.me>
2025-09-28 13:05:56 +0200

    feature: add service worker e2e test (#558)

--- packages/e2e/src/typescript.completion-service-worker.ts
+++ packages/e2e/src/typescript.completion-service-worker.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.completion-service-worker'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/completion-service-worker')
