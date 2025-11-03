6be88e0830d84964dbf16ad47c05357fb0088049
Le Vivilet <levivilet@pm.me>
2025-11-03 18:40:18 +0100

    rename

--- packages/e2e/src/typescript.definition-node-core-module.ts
+++ packages/e2e/src/typescript.definition-node-core-module.ts
@@ -1,12 +1,12 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'typescript.completion-node-core-module'
+export const name = 'typescript.definition-node-core-module'
 
 export const skip = 1
 
 export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
-  const fixtureUrl = import.meta.resolve('../fixtures/completion-node-core-module')
+  const fixtureUrl = import.meta.resolve('../fixtures/definition-node-core-module')
   const workspaceUrl = await FileSystem.loadFixture(fixtureUrl)
   await Workspace.setPath(workspaceUrl)
   await Main.openUri(`${workspaceUrl}/src/test.ts`)
