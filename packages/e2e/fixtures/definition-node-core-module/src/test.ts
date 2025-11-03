0718425d8f9c98baa6b7b102768bfee4dbe25326
Le Vivilet <levivilet@pm.me>
2025-11-03 18:42:51 +0100

    feature: add node e2e test fixture (#592)
    * feature: add node e2e test fixture
    
    * skip
    
    * rename
    
    * rename

---
+++ packages/e2e/fixtures/definition-node-core-module/src/test.ts
@@ -0,0 +1,3 @@
+import { join } from 'node:path'
+
+const joined = join('/test', 'file.txt')
---
+++ packages/e2e/src/typescript.definition-node-core-module.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'typescript.definition-node-core-module'
+
+export const skip = 1
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
+  // arrange
+  const fixtureUrl = import.meta.resolve('../fixtures/definition-node-core-module')
+  const workspaceUrl = await FileSystem.loadFixture(fixtureUrl)
+  await Workspace.setPath(workspaceUrl)
+  await Main.openUri(`${workspaceUrl}/src/test.ts`)
+  await Editor.setCursor(0, 24)
+
+  // act
+  await Editor.goToDefinition()
+
+  // assert
+  const mainTabs = Locator('.MainTab')
+  await expect(mainTabs).toHaveCount(2)
+  const mainTabTwo = mainTabs.nth(1)
+  await expect(mainTabTwo).toHaveText('path.d.ts')
+}
