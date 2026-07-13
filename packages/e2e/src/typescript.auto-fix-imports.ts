80c8b4d4b33cc5c92f01c2ecf9014254603533b0
Le Vivilet <levivilet@pm.me>
2026-07-13 15:51:45 +0200

    Enable TypeScript auto-fix imports e2e test (#617)
    * test: enable auto fix imports e2e test
    
    * feature: update server to 0.87.3

--- packages/e2e/src/typescript.auto-fix-imports.ts
+++ packages/e2e/src/typescript.auto-fix-imports.ts
@@ -2,10 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.auto-fix-import'
 
-// TODO enable when source-action widgets support isolated code-action providers
-export const skip = 1
-
-export const test: Test = async ({ Editor, expect, FileSystem, Locator, Main, Workspace }) => {
+export const test: Test = async ({ Command, Editor, expect, FileSystem, Locator, Main, Workspace }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/auto-fix-imports')
   const workspaceUrl = await FileSystem.loadFixture(fixtureUrl)
@@ -19,11 +16,11 @@ export const test: Test = async ({ Editor, expect, FileSystem, Locator, Main, Wo
   // assert
   const organizeImportsAction = Locator('.SourceActionItem', { hasText: 'Organize Imports' })
   await expect(organizeImportsAction).toBeVisible()
-  // @ts-ignore
-  // eslint-disable-next-line e2e/no-direct-click
-  await organizeImportsAction.click()
+  await Command.execute('EditorSourceAction.selectItem', 'Organize Imports')
 
   // assert
+  await Editor.shouldHaveText(`import { add } from './math.ts'
 
-  // TODO verify that unused imports  have been removed
+add(1, 2)
+`)
 }
