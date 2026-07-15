68788a0cb8caf25d48f7d902a90cca01e27342c5
Le Vivilet <levivilet@pm.me>
2026-07-15 11:00:17 +0200

    feature: enable problems panel e2e test (#547)
    * feature: enable problems panel e2e test
    
    * fix: update problems panel diagnostics test

--- packages/e2e/src/typescript.diagnostics-problems-panel.ts
+++ packages/e2e/src/typescript.diagnostics-problems-panel.ts
@@ -2,11 +2,12 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'typescript.diagnostics-problems-panel'
 
-export const test: Test = async ({ FileSystem, Workspace, Main, Panel, Problems, Locator, expect }) => {
+export const test: Test = async ({ expect, FileSystem, Locator, Main, Panel, Problems, Settings, Workspace }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/diagnostics')
   const workspaceUrl = await FileSystem.loadFixture(fixtureUrl)
   await Workspace.setPath(workspaceUrl)
+  await Settings.update({ 'editor.diagnostics': true })
 
   // act
   await Main.openUri(`${workspaceUrl}/src/test.ts`)
@@ -14,16 +15,9 @@ export const test: Test = async ({ FileSystem, Workspace, Main, Panel, Problems,
   // assert
   await Panel.open('Problems')
   await Problems.show()
-
   const problems = Locator('.Problem')
   await expect(problems).toHaveCount(2)
-
   const problemInfo = problems.nth(1)
   const label = problemInfo.locator('.Label')
   await expect(label).toHaveText(`Type 'string' is not assignable to type 'number'.`)
-
-  // act
-  // TODO
-  // 1. run diagnostics
-  // 2. validate diagnostics are as expected
 }
