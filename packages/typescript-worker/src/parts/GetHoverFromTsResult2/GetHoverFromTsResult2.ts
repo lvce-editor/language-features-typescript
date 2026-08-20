1a969afcb7046a038c5c688f1b3db649f53f4285
Le Vivilet <levivilet@pm.me>
2026-08-20 15:07:22 -0700

    fix: convert TypeScript hover documentation to text (#672)

--- packages/typescript-worker/src/parts/GetHoverFromTsResult2/GetHoverFromTsResult2.ts
+++ packages/typescript-worker/src/parts/GetHoverFromTsResult2/GetHoverFromTsResult2.ts
@@ -1,14 +1,16 @@
 import type ts from 'typescript'
 
+const displayPartsToString = (parts: readonly ts.SymbolDisplayPart[] = []): string => {
+  return parts.map((part) => part.text).join('')
+}
+
 export const getHoverFromTsResult2 = (tsResult: ts.QuickInfo | undefined) => {
   if (!tsResult) {
     return undefined
   }
-  const displayStringParts = tsResult.displayParts || []
-  const displayString = displayStringParts.map((item) => item.text).join('')
   return {
-    displayString: displayString,
-    documentation: tsResult.documentation,
+    displayString: displayPartsToString(tsResult.displayParts),
+    documentation: displayPartsToString(tsResult.documentation),
     languageId: 'typescript',
   }
 }
